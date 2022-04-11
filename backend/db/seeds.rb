edfcaef9bb9d1881b8fd3699a83c0236ec3f82d0
daniloinacio <daniloinaciossilva@gmail.com>
2022-04-11 10:11:25 -0300

    Add seeds de membros da turma

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,7 +11,29 @@ file = File.read('./db/classes.json')
 classes_data = JSON.parse(file)
 
 for data in classes_data do
-  subject = Subject.create(code: data['code'], name: data['name'])
+  subject = Subject.create!(code: data['code'], name: data['name'])
   class_info = data['class']
-  Cclass.create(code: class_info['classCode'], semester: class_info['semester'], time: class_info['time'], subject: subject)
+  Cclass.create!(code: class_info['classCode'], semester: class_info['semester'], time: class_info['time'], subject: subject)
 end
+
+file = File.read('./db/class_members.json')
+class_members_data = JSON.parse(file)
+data = class_members_data[0]
+discentes = data['dicente']
+
+cclass = Cclass.all.first
+
+for discente in discentes do
+  member = Member.create(name: discente['nome'], course: discente['curso'],
+  registration: discente['matricula'], username: discente['usuario'],
+  degree: discente['formacao'], occupation: discente['ocupacao'],
+  email: discente['email'])
+  Enrollment.create(member: member, cclass: cclass)
+end
+
+docente = data['docente']
+member = Member.create(name: docente['nome'], course: docente['departamento'],
+  registration: docente['usuario'], username: docente['usuario'],
+  degree: docente['formacao'], occupation: docente['ocupacao'],
+  email: docente['email'])
+Enrollment.create(member: member, cclass: cclass)
