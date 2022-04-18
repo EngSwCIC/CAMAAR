ab1fe0b7be7b61e5b493c4a5aebe4bf77813b654
Thiago574 <thiagoeliasdosreis01@gmail.com>
2022-04-18 20:33:22 -0300

    #10 Funcionalidade Filtro thiago

---
+++ frontend/src/components/StudentSubjectItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md" style="width: 100%; padding: 0; padding-inline:8px;">
+    <q-list bordered class="rounded-borders">
+      <q-expansion-item
+        expand-separator
+        label="CICXXXX — SUBJECT"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+            eveniet doloribus ullam aliquid.
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ frontend/src/pages/StudentSubjectsPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>subjects:</h1>
+    <StudentSubjectItem/>
+    <StudentSubjectItem/>
+    <StudentSubjectItem/>
+  </div>
+</template>
+
+<script setup>
+  import StudentSubjectItem from 'src/components/StudentSubjectItem.vue'
+</script>
