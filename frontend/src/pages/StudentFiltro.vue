4ae8b32c317c36b9658aa15ca8b6a90463a1a6bb
Mateus <mateusoliveirap@hotmail.com>
2022-04-21 18:07:38 -0300

    Filtro por nome e código. #10

--- frontend/src/pages/StudentFiltro.vue
+++ frontend/src/pages/StudentFiltro.vue
@@ -1,34 +1,18 @@
 <template>
   <div fullscreen>
-    <h3>Pesquisa/Filtro</h3>
-    <!-- passar as disciplinas para o componente filho e mostrar na tela -->
-    <StudentSubjectItem v-for="(subject, idx) in subjects" v-bind:key="idx" :subject="subject" />
+    <StudentFilter />
   </div>
 </template>
-
 <script setup>
-  import StudentSubjectItem from 'src/components/FiltroRAlunoCard.vue'
-  import { ref, onMounted } from 'vue';
-  import axios from "axios";
-  // obter lista de disciplinas do backend
-  const subjects = ref(["a"])
-  onMounted(async () => {
-    await axios.get("http://localhost:3030/subjects/index")
-      .then(resp => {
-        subjects.value = resp.data
-      })
-      .catch(err => {
-          console.error(err);
-      });
-  })
+import StudentFilter from "src/components/FiltroRAlunoCard.vue";
 </script>
 
 <style>
-  h3 {
-    padding: 0;
-    font-size: 2.5em;
-    font-weight: 500;
-    margin: 64px;
-    margin-bottom: 32px;
-  }
+h3 {
+  padding: 0;
+  font-size: 2.5em;
+  font-weight: 500;
+  margin: 64px;
+  margin-bottom: 32px;
+}
 </style>
