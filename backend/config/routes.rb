f0b3b1b983867a99141eb0c9fbb087dee83a5044
Luiz Schonarth <luizcschonarth@gmail.com>
2022-04-22 23:46:18 -0300

    Remove rotas desnecessárias

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :survey_answers
-  resources :likert_questions
-  resources :options
-  resources :likert_scales
-  resources :multiple_choices
-  resources :survey_questions
   resources :surveys
   devise_for :user, :path => '/auth', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" },
   controllers: {
