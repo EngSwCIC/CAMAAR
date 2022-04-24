1c1eced2eb931e1c151469288e3b3d8210d1d134
gustavo-oo <gustavopchaves@hotmail.com>
2022-04-23 23:01:22 -0300

    Testado Option

--- backend/spec/models/option_spec.rb
+++ backend/spec/models/option_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Option, type: :model do
+  before(:each) do
+    @survey = Survey.new(name:"Survey", description:"description", semester:"2021.2")
+    @survey_question = SurveyQuestion.new(question: 'Pergunta', optional: true, survey: @survey)
+  end
+
   context 'Checar criação de Opções para o Questionário' do
     it 'Quando a questão associada é do tipo caixa de seleção' do
-      @survey = Survey.new(name:"Survey", description:"description", semester:"2021.2")
-      @survey_question = SurveyQuestion.new(question_type: 'Caixa de Seleção', question: 'Pergunta', optional: true, survey: @survey)
-
+      @survey_question.question_type = 'Caixa de Seleção'
       option = Option.new(option: "Opção", survey_question: @survey_question)
       expect(option).to be_valid
     end
 
     it 'Quando a questão associada não é do tipo caixa de seleção' do
-      @survey = Survey.new(name:"Survey", description:"description", semester:"2021.2")
-      @survey_question = SurveyQuestion.new(question_type: 'Escala', question: 'Pergunta', optional: true, survey: @survey)
-
+      @survey_question.question_type = 'Escala'
       option = Option.new(option: "Opção", survey_question: @survey_question)
       expect(option).to_not be_valid
     end
