f734287340929ac06745ba5cf9c3d1b71faa7970
gustavo-oo <gustavopchaves@hotmail.com>
2022-04-23 22:48:24 -0300

    Testado SurveyAnswer, SurveyQuestion e Option

--- backend/app/models/option.rb
+++ backend/app/models/option.rb
@@ -3,4 +3,11 @@ class Option < ApplicationRecord
   has_many :selected_options
 
   validates :option, presence: true
+  validate :questionIsOption
+
+  def questionIsOption
+    if(self.survey_question.question_type != "Caixa de Seleção")
+      errors.add(:survey_question, "Questão não é do tipo Caixa de Seleção")
+    end
+  end
 end
--- backend/spec/models/option_spec.rb
+++ backend/spec/models/option_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Option, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Checar criação de Opções para o Questionário' do
+    it 'Quando a questão associada é do tipo caixa de seleção' do
+      @survey = Survey.new(name:"Survey", description:"description", semester:"2021.2")
+      @survey_question = SurveyQuestion.new(question_type: 'Caixa de Seleção', question: 'Pergunta', optional: true, survey: @survey)
+
+      option = Option.new(option: "Opção", survey_question: @survey_question)
+      expect(option).to be_valid
+    end
+
+    it 'Quando a questão associada não é do tipo caixa de seleção' do
+      @survey = Survey.new(name:"Survey", description:"description", semester:"2021.2")
+      @survey_question = SurveyQuestion.new(question_type: 'Escala', question: 'Pergunta', optional: true, survey: @survey)
+
+      option = Option.new(option: "Opção", survey_question: @survey_question)
+      expect(option).to_not be_valid
+    end
+  end
 end
