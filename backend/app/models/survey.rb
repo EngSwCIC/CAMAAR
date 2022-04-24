93d70f515ffac94d7f60b225da16b33c1338d563
gustavo-oo <gustavopchaves@hotmail.com>
2022-04-23 21:01:07 -0300

    Testes Rspec Survey e SurveyQuestion

--- backend/app/models/survey.rb
+++ backend/app/models/survey.rb
@@ -1,4 +1,6 @@
 class Survey < ApplicationRecord
     has_many :survey_question, dependent: :destroy
     has_many :survey_answer, dependent: :destroy
+
+    validates :semester, format: {with: /\d{4}.\d/}
 end
