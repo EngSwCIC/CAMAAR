a42d8c1c1aba77a05e641ea7b9670206f2c0f111
David Mendes <davidmendes@pop-os.localdomain>
2022-04-23 21:28:11 -0300

    :white_check_mark: Testes para metodo member_classes

--- backend/app/controllers/cclasses_controller.rb
+++ backend/app/controllers/cclasses_controller.rb
@@ -3,7 +3,7 @@ class CclassesController < ApplicationController
   # TODO: Validar o usuário logado usando o token, então trocar params[:member_id] pelo current_user.id (remover tambem o parametro da rota)
   def member_classes
     classes = Member.find(params[:member_id]).cclasses
-    render json: classes, status: :ok
+    render json: classes, only: %w[id code semester time], include: { subject: { only: %w[code name] } }, status: :ok
   rescue StandardError
     render json: { message: "Membro não encontrado" } , status: :not_found
   end
