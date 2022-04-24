c4972e7e02ef0fbadcfbbd154cdf03a486ca3734
Igor Cardoso <iygorsilva@gmail.com>
2022-04-23 21:17:54 -0300

    dropdown initial creation

--- frontend/src/components/FilteringCard.vue
+++ frontend/src/components/FilteringCard.vue
@@ -1,34 +1,55 @@
-<script setup>
-function searchSurvey() {
-    return 0;
-}
-</script>
-
-<style>
-#professorSurveySearch {
-  background-image: url('/css/searchicon.png'); /* Add a search icon to input */
-  background-position: 10px 12px; /* Position the search icon */
-  background-repeat: no-repeat; /* Do not repeat the icon image */
-  width: 100%; /* Full-width */
-  font-size: 16px; /* Increase font-size */
-  padding: 12px 20px 12px 40px; /* Add some padding */
-  border: 1px solid #ddd; /* Add a grey border */
-  margin-bottom: 12px; /* Add some space below the input */
-}
-
-#myUL {
-  /* Remove default list styling */
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-}
-</style>
-
 <template>
-    <body>
-         <input type="text" id="professorSurveySearch" onkeyup="searchSurvey()" placeholder="Procurar questionários...">
-        <ul id="myUL">
-            <filtered-card>
-        </ul> 
-    </body>
+  <div class="q-pa-md">
+    <q-btn-dropdown color="primary" label="Dropdown Button">
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Photos</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Videos</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Articles</q-item-label>
+          </q-item-section>
+        </q-item>
+
+
+      </q-list>
+    </q-btn-dropdown>
+  </div>
 </template>
+
+<script>
+import api from "src/services/api.js";
+
+export default {
+  setup() {
+    return {
+      onItemClick() {
+        // console.log('Clicked on an Item')
+      },
+    };
+  },
+
+  mounted() {
+    api.get("/surveys", {
+        params: {
+          // class_id: 1
+        },
+      })
+      .then((response) => {
+        console.log('>>>', response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <ReportDownload />
+    <FilteringCard />
   </div>
 </template>
 
 <script setup>
-import ReportDownload from '/src/components/ReportDownload.vue'
+import ReportDownload from '/src/components/ReportDownload.vue';
+import FilteringCard from 'src/components/FilteringCard.vue';
 </script>
