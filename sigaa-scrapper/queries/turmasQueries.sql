1960de1178bed9147bfc2a485e51f806f57e83ca
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2022-05-01 12:34:52 -0300

    Delete sigaa-scrapper directory

--- sigaa-scrapper/queries/turmasQueries.sql
+++
@@ -1,8 +0,0 @@
-create table turmas (
-    id int auto_increment not null primary key,
-    codigo_materia int not null,
-    codigo varchar(255) unique,
-    semestre varchar(255),
-    horario varchar(255),
-    foreign key (codigo_materia) references materias(id)
-);
