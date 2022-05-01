d3f06da7eefe0978137533f1454cb84a07ce6998
Luiz Schonarth <luizcschonarth@gmail.com>
2022-05-01 15:32:08 -0300

    Corrige requisição ao backend

--- frontend/src/pages/SurveyPage.vue
+++ frontend/src/pages/SurveyPage.vue
@@ -5,16 +5,14 @@
 <script>
     import axios from 'axios'
 
-    console.log('eu existo')
-
     export default {
     data() {
         return { survey: null };
     },
     mounted() {
         axios.get('http://localhost:3000/surveys/' + this.$route.params.id)
-        .then((res) => console.log(res))
-        .finally(() => console.log(this.survey))
+        .then((res) => this.survey = res.data)
+        .catch((err) => console.error(err))
     }
     };
 </script>
