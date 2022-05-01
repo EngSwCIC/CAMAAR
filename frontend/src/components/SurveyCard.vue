1cbc4f46a870bde87b37daa3f749c6817a4766a8
anapaulaonc <anapaulaoncosta@gmail.com>
2022-05-01 14:24:29 -0300

    pagina principal e questionarios

---
+++ frontend/src/components/SurveyCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+
+  // methods
+</script>
+<template>
+<div class="row">
+  <q-card class="my-card">
+   <q-card-section class="bg-purple text-white">
+        <div class="text-h6">Questionário 1</div>
+        <div class="text-subtitle2">por Professor Fulano</div>
+      </q-card-section>
+
+      <q-separator></q-separator>
+
+      <q-card-actions align="right">
+        <q-btn flat>Editar</q-btn>
+        <q-btn flat>Acessar</q-btn>
+      </q-card-actions>
+  </q-card>
+</div>
+</template>
+
+<style scoped>
+.hsize {
+  height: 500px;
+}
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+  
+.rounded-border{
+  border-radius: 25px;
+}
+</style>
