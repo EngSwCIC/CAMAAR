0ef8d4f5baa883661a4ebcc67e958fbeed369b08
AnaJordao <acjpjvjp@gmail.com>
2022-05-03 17:03:46 -0300

    Testes do metodo show do scrapper_controller terminados

--- backend/spec/controllers/scrapper_spec.rb
+++ backend/spec/controllers/scrapper_spec.rb
@@ -49,7 +49,11 @@ RSpec.describe ScrapperController, :type => :controller do
             expect(response.body).to eq("{\"classes\":{\"classes\":[{\"code\":\"CIC0097\",\"name\":\"BANCOS DE DADOS\",\"class\":{\"classCode\":\"TA\",\"semester\":\"2021.2\",\"time\":\"35T45\"}},{\"code\":\"CIC0105\",\"name\":\"ENGENHARIA DE SOFTWARE\",\"class\":{\"classCode\":\"TA\",\"semester\":\"2021.2\",\"time\":\"35M12\"}},{\"code\":\"CIC0202\",\"name\":\"PROGRAMAÇÃO CONCORRENTE\",\"class\":{\"classCode\":\"TA\",\"semester\":\"2021.2\",\"time\":\"35M34\"}}]}}")
         end
 
-
+        it 'should not return a empty response' do
+          get :show
+          expect(response).to have_http_status(200)
+          expect(response.body).not_to be nil
+        end
     end
 
 end
