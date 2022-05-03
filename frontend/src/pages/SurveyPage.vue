94dfc3844caf9171cc1a4b74af5911dd852aca0f
Luiz Schonarth <luizcschonarth@gmail.com>
2022-05-03 15:06:45 -0300

    Adiciona botão de submit de resposta

--- frontend/src/pages/SurveyPage.vue
+++ frontend/src/pages/SurveyPage.vue
@@ -3,6 +3,9 @@
     <h2> {{ survey.name }} </h2>
     <p> {{ survey.description }} </p>
     <div v-for="(cclass, c_index) in cclasses" :key="cclass.id">
+        <div>
+            <h4>{{ cclass.subject.code + ' - ' + cclass.subject.name }}</h4>
+        </div>
         <div v-for="(question, q_index) in survey.survey_questions" :key="question.id" >
             <SurveyQuestion
                 :question="question"
@@ -11,16 +14,23 @@
             />
         </div>
     </div>
+    <q-btn
+        @click="submitAnswer"
+        rounded
+        color="secondary"
+    >Enviar</q-btn>
 </div>
 </template>
 
 <script>
 import SurveyQuestion from '../components/SurveyQuestion.vue'
 import axios from 'axios'
+import { useQuasar } from 'quasar'
+import { useRouter } from 'vue-router'
 
 export default {
     components: {
-        SurveyQuestion
+        SurveyQuestion,
     },
     data() {
         return {
@@ -29,13 +39,34 @@ export default {
         survey_answers: []
         };
     },
+    setup() {
+        const $q = useQuasar()
+        const router = useRouter()
+
+        const notify = (param) => {
+            $q.notify(param)
+        }
+
+        return {
+            notify,
+            router
+        }
+    },
     async mounted() {
-        // const credentials = creadentialsStore()
+        try {
+            const res_s = await axios.get('http://localhost:3000/surveys/' + this.$route.params.id)
+            this.survey = res_s.data
+        } catch(err) {
+            console.error(err)
+            this.notify({
+                message: 'Erro ao carregar questionário.',
+                position: 'top',
+                color: 'negative'
+            })
+            this.router.push({path: '/home'})
+        }
 
-        const res_s = await axios.get('http://localhost:3000/surveys/' + this.$route.params.id)
-        this.survey = res_s.data
-
-        // Temporariamente usa user.id = 1 para protótipo do questionário
+        // Temporariamente usa id de membro = 1 para protótipo do questionário
         const res_c = await axios.get(`http://localhost:3000/members/${1}/cclasses`)
         this.cclasses = res_c.data
 
@@ -65,6 +96,37 @@ export default {
                })
            })
         }); 
+    },
+    methods: {
+        async submitAnswer() {
+            this.survey_answers.forEach(async subject_answer => {
+                try {
+                    const res = await axios.post(
+                        'http://localhost:3000/survey_answers',
+                        subject_answer, { 
+                            headers: {
+                                Authorization: 'Bearer ' + localStorage.getItem('token')
+                            }
+                        })
+
+                    this.notify({
+                        message: 'Resposta enviada com sucesso!',
+                        position: 'top',
+                        color: 'positive'
+                    })
+            
+                    this.router.push({path: '/home'})
+                } catch(err) {
+                    this.notify({
+                        message: 'Falha ao enviar resposta.',
+                        position: 'top',
+                        color: 'negative'
+                    })
+                }
+                
+
+            })
+        }
     }
 };
 </script>
