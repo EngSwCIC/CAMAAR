d5068fb7132b49e3b02d5eac4a8884a90a007236
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2022-05-04 20:11:12 -0300

    filtrando funcionando

--- frontend/src/components/FilteringCclasses.vue
+++ frontend/src/components/FilteringCclasses.vue
@@ -1,33 +1,33 @@
 <template>
 <div class="card text-center m-3">
-    <q-btn-dropdown color="pink" label="Escolher turma" dropdown-icon="change_history" @click="chooseCclass($event)">
+    <q-btn-dropdown color="pink" label="Escolher turma" dropdown-icon="change_history">
       <q-list v-for="item in cclassess" :key="item.id">
             <q-list v-for="itemx in item" :key="itemx.id">
-                <q-item clickable v-close-popup :value="itemx['id']">
-                <q-item-section>
-                    <q-item-label> {{ itemx['attributes']['id'] +" => " + itemx['attributes']['code']}}</q-item-label>
+                <q-item clickable v-close-popup>
+                <q-item-section @click="chooseCclass(itemx['attributes']['code'])">
+                    <q-item-label > {{ itemx['attributes']['id'] +" => " + itemx['attributes']['code']}}</q-item-label>
                 </q-item-section>
                 </q-item>
             </q-list>
       </q-list>
     </q-btn-dropdown>
-    <q-btn-dropdown color="pink" label="Escolher código do Questionário" dropdown-icon="change_history" @click="chooseSurvey($event)">
+    <q-btn-dropdown color="pink" label="Escolher código do Questionário" dropdown-icon="change_history">
       <q-list v-for="item in surveyss" :key="item.id">
             <q-list v-for="itemx in item" :key="itemx.id">
-                <q-item clickable v-close-popup  :value="itemx['attributes']['code']">
-                <q-item-section>
+                <q-item clickable v-close-popup>
+                <q-item-section @click="chooseSurvey(itemx['attributes']['code'])" >
                     <q-item-label> {{ itemx['attributes']['code'] }}</q-item-label>
                 </q-item-section>
                 </q-item>
             </q-list>
       </q-list>
     </q-btn-dropdown>
-    <q-btn-dropdown color="pink" label="Escolher código do Questão" dropdown-icon="change_history" @click="chooseQuestion($event)" >
+    <q-btn-dropdown color="pink" label="Escolher código do Questão" dropdown-icon="change_history">
       <q-list v-for="item in questionss" :key="item.id">
         <q-list v-for="itemx in item" :key="itemx.id">
-          <q-item clickable v-close-popup :value="itemx['id']">
-            <q-item-section>
-              <q-item-label> {{ itemx['id'] }}</q-item-label>
+          <q-item clickable v-close-popup>
+            <q-item-section @click="chooseQuestion(itemx['id'])">
+              <q-item-label> {{ itemx['id']}} </q-item-label>
             </q-item-section>
           </q-item>
         </q-list>
@@ -48,14 +48,14 @@ export default {
   },
   setup () {
     return {
-      chooseCclass (event) {
-        console.log(event.target.value)
+      chooseCclass (x) {
+        console.log(x)
       },
-      chooseSurvey (event) {
-        console.log(event.target.value)
+      chooseSurvey (x) {
+        console.log(x)
       },
-      chooseQuestion (event) {
-        console.log(event.target.value)
+      chooseQuestion (x) {
+        console.log(x)
       }
     }
   },
