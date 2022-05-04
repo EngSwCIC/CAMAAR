6b31e33d367744cb0c92edeacd586c4726c7456d
Luiz Schonarth <luizcschonarth@gmail.com>
2022-05-03 22:03:05 -0300

    Adiciona validação para respostas de escala de likert

--- backend/app/models/likert_answer.rb
+++ backend/app/models/likert_answer.rb
@@ -1,4 +1,12 @@
 class LikertAnswer < ApplicationRecord
-    belongs_to :answer
-    belongs_to :likert_question
+  belongs_to :answer
+  belongs_to :likert_question
+
+  validate :required_questions_answered
+
+  private 
+
+  def required_questions_answered
+    errors.add(:content, 'is missing') if not answer.survey_question.optional and content.nil?
+  end
 end
