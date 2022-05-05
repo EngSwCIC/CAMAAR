67e05ad92cd673d8a8d0ffb78a1d4ba75c75d8e4
Igor Cardoso <iygorsilva@gmail.com>
2022-05-05 17:39:31 -0300

    fix bug

--- frontend/src/components/GraficoProfessorTipo1.vue
+++ frontend/src/components/GraficoProfessorTipo1.vue
@@ -11,9 +11,9 @@ export default {
             dois: null,
             tres: null,
             quatro: null,
-            cinco: null,                   
-        }        
-    },    
+            cinco: null,
+        }
+    },
     methods: {
         async fetchData() {
             this.dados = null
@@ -24,28 +24,28 @@ export default {
                     this.dois = this.dados[1][1]
                     this.tres = this.dados[2][1]
                     this.quatro = this.dados[3][1]
-                    this.cinco = this.dados[4][1]               
+                    this.cinco = this.dados[4][1]
                 })
                 .catch(error => {
                     console.log(error);
-                });            
-            // const res = await fetch(`https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=1&length=5&type=json`);            
+                });
+            // const res = await fetch(`https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=1&length=5&type=json`);
         }
     },
     mounted() {
         this.fetchData()
     },
-        
+
     name: "ProfessorComponent",
     components: {},
-  
+
 };
 </script>
 
 <template>
     <center>
         <br><br><br><br>
-        <div style="width:40%; height: 40%;">
+        <div v-if="dados"  style="width:40%; height: 40%;">
             {{dados}}
             <column-chart :data="[['&#128560;', um], ['&#128577;', dois], ['&#129300;', tres], ['&#128578;', quatro], ['&#128516;', cinco]]" ytitle="Num. Alunos" ></column-chart>
         </div>
