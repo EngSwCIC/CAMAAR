91dfe1890aedf60a9339195b6e640bb807f6fe65
Igor Cardoso <iygorsilva@gmail.com>
2022-05-05 17:27:01 -0300

    fix error in request

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://127.0.0.1:3000",
-  headers: ('Access-Control-Allow-Origin: *'),
+  baseURL: "http://127.0.0.1:3000",
+  // headers: ('Access-Control-Allow-Origin: *'),
 
   // baseURL: "https://canvasjs.com",
 });
