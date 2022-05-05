157c1bc9adb08ec73349a875a4b98e0a89aa5da6
David Mendes <davidmendes@pop-os.localdomain>
2022-05-05 01:51:30 -0300

    :adhesive_bandage: arrumando validations selected_options

--- backend/app/models/selected_option.rb
+++ backend/app/models/selected_option.rb
@@ -1,5 +1,5 @@
 class SelectedOption < ApplicationRecord
-  belongs_to :option, optional: true
+  belongs_to :option, optional: { scope: :custom_option }
   belongs_to :answer
 
   validates :custom_option, presence: true, unless: :option
