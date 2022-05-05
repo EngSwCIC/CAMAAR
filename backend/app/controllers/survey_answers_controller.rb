6c759c7a6ec2c59a8c12826443d1072717d8be8e
David Mendes <davidmendes@pop-os.localdomain>
2022-05-04 23:46:47 -0300

    :sparkles: criando as selected options para questoes de caixa de selecao

--- backend/app/controllers/survey_answers_controller.rb
+++ backend/app/controllers/survey_answers_controller.rb
@@ -4,9 +4,20 @@ class SurveyAnswersController < ApplicationController
     survey_answers = SurveyAnswer.new(enrollment: enrollment, survey_id: params[:survey_id])
     survey_answers.save!
     params.require(:answers).each do |answer|
-      a = Answer.new(answer.permit(:survey_question_id, :content))
+      a = Answer.new(answer.permit(:survey_question_id, :content)) #(:selected_options, :custom_option)
       a.survey_answer_id = survey_answers.id
       a.save!
+      if a.survey_question.question_type == 'Caixa de Seleção'
+        options = answer.require(:selected_options).map(&:to_i)
+        custom_option = answer.require(:custom_option)
+
+        options.each do |option|
+          SelectedOption.create(option_id: option, answer_id: a.id)
+        end
+
+        SelectedOption.create(custom_option: custom_option, answer_id: a.id) if custom_option
+      end
+
     end
     head(:created)
   rescue StandardError => e
