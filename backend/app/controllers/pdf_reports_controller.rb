adadc7489e4dd704bab9820e5cac497dcf65b244
jvmaia <joaovmferreira@gmail.com>
2022-05-05 22:36:07 -0300

    remove conflict

--- backend/app/controllers/pdf_reports_controller.rb
+++ backend/app/controllers/pdf_reports_controller.rb
@@ -2,14 +2,6 @@ class PdfReportsController < ApplicationController
   # controller for generating pdf
 
   def index
-<<<<<<< HEAD
-    @surveys = params[:class_id] ? Survey.find(params[:class_id]) : Survey.all
-    @answers = params[:class_id] ? Answer.joins(:member).find(params[:class_id]) : Answer.joins(:member).all
-    @questions = params[:class_id] ? Question.find(params[:class_id]) : Question.all
-
-    render json: { surveys: @surveys, answers: @answers, questions: @questions }, status: :ok
-
-=======
     @data = "\n\n\t\tRelatório\n\n"
     Cclass.all.each do |c|
       s = Survey.includes(:cclass).find_by(cclass: c)
@@ -18,6 +10,5 @@ class PdfReportsController < ApplicationController
 
     render json: { data: @data }
     end
->>>>>>> 295a2c24abc25a41500fd5d58bba1ef5056a2b22
   end
 end
