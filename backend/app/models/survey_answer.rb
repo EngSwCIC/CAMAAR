8f70b65862f400fa3b9866ec46748bb7b4cafd8c
daniloinacio <daniloinaciossilva@gmail.com>
2022-06-27 21:45:58 -0300

    Remove a validacao de estudante da survey answers

--- backend/app/models/survey_answer.rb
+++ backend/app/models/survey_answer.rb
@@ -9,8 +9,6 @@ class SurveyAnswer < ApplicationRecord
   has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers
 
-  validate :is_grad_student
-
   ##
   # Método que sobrescreve funcionalidade padrão de +as_json+ do +ActiveRecord+
   # a fim de incluir atributos de +Answers+ e +LikertAnswers+ aninhados
@@ -21,14 +19,4 @@ class SurveyAnswer < ApplicationRecord
 
   private
 
-  ##
-  # Valida se +occupation+ do membro que está respondendo o Survey é do tipo _"dicente"_
-  #
-  # É chamada durante a execução do método +SurveyAnswer:save+.
-  #
-  # Adiciona um erro ao model caso +member.occupation != 'dicente'+ 
-
-  def is_grad_student
-    errors.add(:member, 'membro deve ser dicente') unless member&.occupation == 'dicente'
-  end
 end
