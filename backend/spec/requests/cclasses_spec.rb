7a7af7d13befb0f8845429e3ba69974712459174
Maycon Fabio <m.vinnicyus@gmail.com>
2022-08-25 00:02:48 -0300

    Adiciona testes BDD da feature de relatórios

--- backend/spec/requests/cclasses_spec.rb
+++ backend/spec/requests/cclasses_spec.rb
@@ -26,5 +26,21 @@ RSpec.describe "Cclasses", type: :request do
       expect(response_body[44]['name']).to eq('MARISTELA TERTO DE HOLANDA')
     end
   end
+  
+  describe "GET /performance" do
+    it "should throw an error if semester is not in request" do
+      pending
+      get "/cclasses/performance"
+    end
 
+    it "should throw an error cclasses performances if user is not admin" do
+      pending
+      get "/cclasses/performance?semester=2021.2"
+    end
+
+    it "returns cclasses performances in 2021.2 semester" do
+      pending
+      get "/cclasses/performance?semester=2021.2"
+    end
+  end
 end
