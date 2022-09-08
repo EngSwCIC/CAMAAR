89c2bb2ca666d5f6e0a7254fced831ec1973cecf
Lucca Huguet <luccahuguet@hotmail.com>
2022-09-08 18:56:48 -0300

    v4

---
+++ frontend/src/components/SurveyCoordCard.vue
@@ -0,0 +1,40 @@
+<script>
+import { useRouter } from "vue-router";
+export default {
+  props: ["survey"],
+  setup() {
+    const router = useRouter();
+
+    return {
+      router,
+    };
+  },
+  methods: {
+    redirectCreateSurvey() {
+      this.router.push({ path: "/surveys/create" });
+    },
+  },
+};
+</script>
+<template>
+  <div class="row">
+    <q-card class="rounded-border q-pa-xl hsize justify-center text-center">
+      <q-space></q-space>
+      <br />
+      <div class="text-h5">Menu</div>
+      <br />
+      <q-btn color="secondary" @click="redirectCreateSurvey">
+        Criar Questionários
+      </q-btn>
+    </q-card>
+  </div>
+</template>
+
+<style scoped>
+.hsize {
+  height: 500px;
+}
+.rounded-border {
+  border-radius: 25px;
+}
+</style>
