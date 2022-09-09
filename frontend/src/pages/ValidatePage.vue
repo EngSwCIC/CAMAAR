639f0899b1984491bc36fa1142d2b88c32fa1fcd
Bruno Barros Xavier <200015621@aluno.unb.br>
2022-09-09 13:54:00 -0300

    Revert "Styling ValidatePage"
    This reverts commit afd979d069b1d585d322ed195b59cfa3f1c13341.
    
    Conta errada

--- frontend/src/pages/ValidatePage.vue
+++ frontend/src/pages/ValidatePage.vue
@@ -1,33 +1,10 @@
 <template>
-	<div class="bg-secondary fullscreen row items-center justify-center">
-	<div class="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-4">
-	<q-card class="rounded-border q-pa-xl hsize justify-center col-12 text-center">
-      <h1 class="text-h3">Validação de cadastro</h1>
-      <q-form ref="form" class="q-gutter-sm q-mt-xl row">
+	<h3>
+		Validação de cadastro
+	</h3>
 
-		<q-input 
-			outlined 
-			rounded
-			label="Codigo" 
-			v-model="codigoInput" 
-			placeholder="Digite seu código aqui" 
-			class="col-12"
-        	lazy-rules
-		/>
-			
-		<q-btn 
-			rounded
-        	color="secondary"
-        	class="col-12"
-        	size="lg"
-			@click.prevent="validatePost" 
-			label="Validar código"
-		/>
-	  
-      </q-form>
-    </q-card>
-	</div>
-	</div>
+	<q-input outlined v-model="codigoInput" label="Codigo" placeholder="Digite seu código aqui" />
+	<q-btn @click.prevent="validatePost" label="Validar código"/>
 </template>
 
 <script>
@@ -55,12 +32,3 @@ export default {
 	}
 }
 </script>
-
-<style scoped>
-.hsize {
-  height: 500px;
-}
-.rounded-border{
-  border-radius: 25px;
-}
-</style>
