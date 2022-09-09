a9e1aba89b0ce47537aebae001bd350846a77a86
Maycon Fabio <m.vinnicyus@gmail.com>
2022-09-09 19:55:07 -0300

    feat: Add base para a página de reports

---
+++ frontend/src/pages/ReportsPage.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      surveys: null,
+    };
+  },
+  async mounted() {
+    const res = await this.$axios.get("/api/surveys/open");
+    this.surveys = res.data;
+    console.log(res);
+    // .then((res) => (this.surveys = res.data))
+    // .finally(() => console.log(this.surveys));
+  },
+};
+</script>
+<template>
+  <h1>OI</h1>
+</template>
