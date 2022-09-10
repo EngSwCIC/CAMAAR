fbb461e0d77a113dc9079746642b8e91f86a1755
Bruno Barros Xavier <BrunoBarrosXavier@proton.me>
2022-09-09 22:44:27 -0300

    View of Validate by Link

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,3 +1,4 @@
+
 import LoginPage from '../pages/LoginPage.vue'
 import ImportPage from '../pages/ImportPage.vue'
 import HomePage from "../pages/HomePage.vue";
@@ -7,12 +8,18 @@ import CadastrarPage from  '../pages/CadastrarPage.vue'
 import StudentClass from  '../pages/StudentClass.vue'
 import StudentEvaluationsReport from '../pages/StudentEvaluationsReport.vue'
 import ValidatePage from '../pages/ValidatePage.vue'
+import ValidateLinkPage from '../pages/ValidateLinkPage.vue'
 
 const routes = [
   {
     path: "/validate",
     name: "Validate",
     component: ValidatePage
+  },
+  {
+    path: "/validate/:id",
+    name: "ValidateLink",
+    component: ValidateLinkPage
   },
   {
     path: "/",
