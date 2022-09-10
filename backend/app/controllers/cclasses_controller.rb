68aaed9bee9a2259be883468618c2b55ce2f810c
Pedro Venzi <pvenzi.pv@gmail.com>
2022-09-09 22:24:21 -0300

    :white_check_mark: Cclass Grades Specs

--- backend/app/controllers/cclasses_controller.rb
+++ backend/app/controllers/cclasses_controller.rb
@@ -52,4 +52,22 @@ class CclassesController < ApplicationController
   rescue StandardError => e
     render json: { message: e }, status: :bad_request
   end
+
+  def cclass_grades
+    cclasses = Cclass.all
+
+    total_grade = 0
+
+    cclass.member_cclass.each do |member_cclass|
+      if member_cclass.grade
+        member_cclass.grade += total_grade
+      end
+    end
+
+    avg_grade = total_grade / (cclass.member_cclass.length)
+
+    render json: avg_grade, status: :ok
+  rescue StandardError => e
+    render json: { message: e }, status: :bad_request
+  end
 end
