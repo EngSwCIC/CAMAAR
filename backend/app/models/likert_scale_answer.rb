b2533622859aa3f7a68c9f0fe8f2dc947627e9b2
Matheus Alves <msantosalves404@gmail.com>
2022-09-09 22:57:53 -0300

    Correção do Controller de QuestionAnswers

--- backend/app/models/likert_scale_answer.rb
+++ backend/app/models/likert_scale_answer.rb
@@ -1,4 +1,28 @@
 class LikertScaleAnswer < ApplicationRecord
     belongs_to :likert_scale_question
     belongs_to :question_answer
+
+    def as_json(options: {})
+        super(
+          only: [:answer]
+        )
+    end
+
+    def self.map_likert_options(option)
+        case option
+            when 'Discordo totalmente'
+                1
+            when 'Discordo parcialmente'
+                2
+            when 'Não sei/Não se aplica'
+                3
+            when 'Concordo parcialmente'
+                4
+            when 'Concordo totalmente'
+                5
+            else
+                "Error"
+        end
+    end
+
 end
