050a4dfddcca15b507ffc215746bb342ccd73273
Lucca Huguet <luccahuguet@hotmail.com>
2022-09-10 21:56:35 -0300

    v9
    Co-authored-by: Hyago Gabriel <hyagohoudini@users.noreply.github.com>

--- frontend/src/pages/CreateSurveyPage.vue
+++ frontend/src/pages/CreateSurveyPage.vue
@@ -1,5 +1,8 @@
 <script>
+import axios from "axios";
 import { ref } from "vue";
+
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
@@ -8,11 +11,16 @@ export default {
     var questionFormat = ref(null);
     var questions = ref([]);
     var multiple_choice_array = ref([]);
+    const router = useRouter();
 
     return {
       questionFormat,
       courses,
       turma,
+      name_quest: ref(null),
+      desc_quest: ref(null),
+      questExpirationDate: ref(null),
+      semester: ref(null),
       options: ["Eng. Software", "PAA", "APC", "OAC"],
       questions,
       text: ref(""),
@@ -22,40 +30,88 @@ export default {
       isOptional: ref(null),
       checkbox_model: ref(null),
       ratingModel: 10, // garante que o likert estará sempre full
-      turmas: ["A", "B", "C"],
+      surveyModel: {
+        name: "",
+        description: "",
+        role_id: 2,
+        expiration_date: "",
+        semester: "",
+        survey_questions_attributes: [],
+      },
+      questionForBack: {
+        question_number: 0,
+        question: "", //enunciado
+        question_type_id: 0, // 1 discursive e etc
+        optional: false,
+        question_options_attributes: [], // multipla escolha
+        likert_scale_questions_attributes: [],
+      },
+      choiceFormat: {
+        question: "",
+        question_number: 0,
+        scale_points: 5,
+      },
       questionTypes: [
         {
           suvey_question_id: 1,
           question_type: "discursive",
           content: null,
           label: "Discursiva",
-          value: 1,
+          id: 1,
         },
         {
           survey_question_id: 2,
           question_type: "multiple_choice",
           content: null,
           label: "Multipla Escolha",
-          value: 2,
+          id: 2,
         },
         {
           survey_question_id: 3,
           question_type: "likert_scale",
           content: null,
           label: "Escala",
-          value: 3,
+          id: 3,
         },
       ],
+      semesters: ["2021.1", "2021.2", "2022.1", "2022.2"],
       mainEvent(e) {
+        var s1 = new Object(this.surveyModel);
+        // turmas: ["A", "B", "C"],
+        // {
+        //   "name": "TESTE DERRADEIRO",
+        //   "description": "this is a test survey",
+        //   "role_id": 2,
+        //   "expiration_date": "Thu, 21 Jun 2028 12:15:50 -0300",
+        //   "semester": "2021/2",}
+
+        s1.name = this.text;
+
+        // const s1 = Object.create(this.surveyModel, {
+        //   alice: { value: 18, enumerable: true },
+        //   bob: { value: 27, enumerable: true },
+        // });
+
+        console.error("my object survey 1 : %o", s1);
+
+        console.log("my object questionFormat: %o", {
+          ...questionFormat.value,
+        });
+
         var question_obj = new Object();
 
         question_obj.content = this.text;
         question_obj.label = questionFormat.value.label;
+        question_obj.id = questionFormat.value.id;
         question_obj.question_type = questionFormat.value.question_type;
         question_obj.isOptional = this.checkbox_model;
 
         if (question_obj.question_type == "likert_scale") {
           question_obj.likert_range = this.likertRange;
+        }
+        if (question_obj.question_type == "multiple_choice") {
+          question_obj.multiple_choice_obj = { ...this.multiple_choice_array };
+          this.multiple_choice_array = [];
         }
 
         questions.value.push(question_obj);
@@ -69,6 +125,33 @@ export default {
         multiple_choice_array.value.push(multi_quest_obj);
 
         this.multiple_choice_text = "";
+      },
+      async createData() {
+        var aux = new Object(this.surveyModel);
+        var question_aux = new Object(this.questionForBack);
+
+        aux.name = this.name_quest;
+        aux.description = this.desc_quest;
+        aux.semester = this.semester;
+        aux.expiration_date = this.questExpirationDate;
+
+        questions.value.forEach(function (item, i) {
+          question_aux.optional = item.isOptional;
+          question_aux.question = item.content;
+          question_aux.question_type_id = item.id;
+          aux.survey_questions_attributes.push(question_aux);
+        });
+
+        const res = await axios.post("/api/surveys/", aux);
+
+        if (res.status == 200) {
+          // await router.push({ path: "/surveys" });
+          alert("Deu bom");
+        } else {
+          alert("Deu ruim");
+        }
+
+        return aux;
       },
     };
   },
@@ -84,11 +167,10 @@ export default {
               src="https://braziljournal.s3.amazonaws.com/covers/45b8fd3d-76a8-2c53-a564-83a394cef7f9.jpg?v=1596499602"
             />
           </template>
-
           <h1 class="text-white">Criação dos Questionários</h1>
         </q-parallax>
         <div class="rounded-border q-pa-xl hsize justify-center col-6">
-          <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <q-form class="q-gutter-md">
             <h3>Selecione a matéria</h3>
             <q-select
               filled
@@ -96,12 +178,35 @@ export default {
               :options="options"
               label="Selecione a matéria"
             />
-            <h3>Selecione a turma</h3>
+            <!-- <h3>Selecione a turma</h3>
             <q-select
               filled
               v-model="turma"
               :options="turmas"
               label="Selecione a turma"
+            /> -->
+
+            <h3>Nome do Questionário</h3>
+            <q-input
+              v-model="name_quest"
+              filled
+              type="textarea"
+              class="form-control"
+            />
+            <h3>Descrição do Questionário</h3>
+            <q-input
+              v-model="desc_quest"
+              filled
+              type="textarea"
+              class="form-control"
+            />
+            <h3>Data de Expiração do Questionário</h3>
+            <q-date v-model="questExpirationDate" minimal />
+            <q-select
+              filled
+              v-model="semester"
+              :options="semesters"
+              label="Selecione o semestre"
             />
             <q-card
               v-for="item in questions"
@@ -110,6 +215,21 @@ export default {
             >
               <div class="text-h6">{{ item.label }}</div>
               <div class="text-subtitle2">{{ item.content }}</div>
+              <div v-if="item.multiple_choice_obj">
+                <q-list
+                  bordered
+                  separator
+                  v-for="(choice, index) in item.multiple_choice_obj"
+                  :key="choice.id"
+                >
+                  <q-item clickable v-ripple>
+                    <q-item-section
+                      >Opção {{ Number(index) + 1 }} :
+                      {{ choice.content }}</q-item-section
+                    >
+                  </q-item>
+                </q-list>
+              </div>
 
               <div v-if="item.isOptional">
                 <input
@@ -129,7 +249,6 @@ export default {
                 color="primary"
               />
             </q-card>
-
             <h3>Inserir nova questão</h3>
             <div>
               <q-select
@@ -152,11 +271,13 @@ export default {
               placeholder="Escreva o enunciado..."
             />
             <q-card
-              v-for="item in multiple_choice_array"
+              v-for="(item, index) in multiple_choice_array"
               class="rounded-border q-pa-xl hsize justify-left text-left"
               :key="item.content"
             >
-              <div class="text-subtitle2">{{ item.content }}</div>
+              <div class="text-subtitle2">
+                Opção {{ index + 1 }} : {{ item.content }}
+              </div>
             </q-card>
             <q-input
               v-if="questionFormat?.question_type == 'multiple_choice'"
@@ -184,11 +305,15 @@ export default {
             <q-btn color="secondary" @click="mainEvent($event)">
               Adicionar questão
             </q-btn>
+            <q-btn color="secondary" @click="cancelQuestEvent($event)">
+              Cancelar
+            </q-btn>
+            <q-btn color="secondary" @click="createData($event)">
+              Finalizar Questionário
+            </q-btn>
           </q-form>
         </div>
       </div>
     </div>
   </div>
 </template>
-<!--  v-if="questionFormat.label == 'likert_scale'"
--->
