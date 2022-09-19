0a9ee8af6d82f0090e834d29a5d5e2f7283a15cb
Maycon Fabio <m.vinnicyus@gmail.com>
2022-09-19 16:13:25 -0300

    refactor: Better queries on reports

--- backend/app/controllers/cclasses_controller.rb
+++ backend/app/controllers/cclasses_controller.rb
@@ -39,70 +39,28 @@ class CclassesController < ApplicationController
     # TODO: melhorar as queries (provavelmente dá pra fazer a mesma coisa com menos queries)
     # TODO: tornar essa rota privada para admins (quando tivermos essa feature)
 
-    @semester = params[:semester]
-    if @semester.nil?
-      @semester = '2021.2'
-    end
-
-    @likert_answers = LikertScaleAnswer
-      .select('id, answer, question_answer_id, likert_scale_question_id')
-
-    # pega a resposta de cada questão de cada membro de questões tipo likert
-    # likert_scale_question_answers: likert_scale_question_id => [answer, ...]
-    @likert_scale_question_answers = Hash.new
-    @likert_answers.each do |answer|
-      if @likert_scale_question_answers[answer.likert_scale_question_id].nil?
-        @likert_scale_question_answers[answer.likert_scale_question_id] = []
-      end
-      @likert_scale_question_answers[answer.likert_scale_question_id]
-        .push(answer.answer)
-    end
-
-
-    @survey_questions = SurveyQuestion
-      .where(question_type_id: 3) # 3 = Likert Scale
-
-    @survey_questions_with_answers = Hash.new
-    @survey_questions.each do |question|
-      # passa por cada questão de um survey e associa as respostas
-      @likert_scale_questions_with_answers = []
-      question.likert_scale_questions.each do |l_question|
-        @likert_scale_questions_with_answers.push({
-          id: l_question.id,
-          question: l_question.question,
-          scale_points: l_question.scale_points,
-          survey_question_id: l_question.survey_question_id,
-          answers: @likert_scale_question_answers[l_question.id]
-        })
-      end
-
-      # adiciona as questões de um survey com suas respostas em um hash
-      @survey_questions_with_answers[question.survey_id] = {
-        id: question.id,
-        survey_id: question.survey_id,
-        question_type_id: question.question_type_id,
-        question: question.question,
-        likert_scale_questions: @likert_scale_questions_with_answers,
-      }
-    end
-
+    # caso o semestre não seja passado, assume o semestre atual
+    @semester = params[:semester] || '2021.2'
 
     @surveys = Survey
       .select('surveys.*')
       .where('surveys.semester': @semester)
-
-    # associa as respostas de cada questão de cada survey
-    @survey_with_question_answers = []
-    @surveys.each do |survey|
-      @survey_with_question_answers.push({
+      .map { |survey| {
         id: survey.id,
         name: survey.name,
-        description: survey.description,
         semester: survey.semester,
-        survey_questions: @survey_questions_with_answers[survey.id]
-      })
-    end
+        survey_questions: survey.survey_questions.where('question_type_id': 3)
+          .map { |question| {
+            question: question.question,
+            likert_scale_questions: question.likert_scale_questions.map { |likert_question| {
+              id: likert_question.id,
+              question: likert_question.question,
+              scale_points: likert_question.scale_points,
+              answers: likert_question.likert_scale_answers.map { |answer| answer.answer }
+            } }
+          } }
+      } }
 
-    render json: @survey_with_question_answers
+    render json: @surveys
   end
 end
