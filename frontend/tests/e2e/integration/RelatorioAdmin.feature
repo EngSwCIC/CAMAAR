0339ca75985a13187bb46acf0b38d39ced60a4c9
BomDia12 <chamatico2@gmail.com>
2022-09-21 21:37:17 -0300

    first definitions

--- frontend/tests/e2e/integration/RelatorioAdmin.feature
+++ frontend/tests/e2e/integration/RelatorioAdmin.feature
@@ -3,13 +3,15 @@ Funcionalidade: Gerar Relatório das notas para o Admin
 
 Cenario: Um usuário não admin tenta acessar o Relatório
 Dado que estou na rota "/admin/relatorio"
-Então eu deveria ver um aviso de "não tenho acesso à página"
+Então eu deveria ser redirecionado para a página de login
 
+@logged
 Cenario: Um usuário admin acessa após o lançamento das informações
 Dado que estou na rota "/admin/relatorio"
 Então eu deveria ver "as médias das turmas", dispostas em formato de tabela
-E as "notas" devem ser separadas por disciplinas
 
+@logged
+@noReportData
 Cenário: Um admin acessa antes do lançamento das notas
 Dado que estou na rota "/admin/relatorio"
 Então devo ver uma mensagem de "não há dados aqui ainda"
--- frontend/tests/e2e/integration/RelatorioAdmin/index.js
+++ frontend/tests/e2e/integration/RelatorioAdmin/index.js
@@ -1,28 +1,29 @@
-import { Given, Then, And } from "cypress-cucumber-preprocessor/steps";
-const { visit, the } = cy;
+import { Given, Then, And, Before } from "cypress-cucumber-preprocessor/steps";
 
-////////////////////////////////////////////////////////////
-Given(`que estou na rota {string}`, (pagina) => {
-    visit(pagina);
-    Then(/^(:?|eu )deveria ver o aviso de"(\w+)"$/, (id) => {
-        the(id);
-      });
-});
+Before({ tags: '@logged' }, () => {
+    // log the user in code
+})
 
-///////////////////////////////////////////////////////////
-Given(`que estou na rota {string}`, (pagina) => {
-    visit(pagina);
-    Then('eu deveria ver as {string}, dispostas em formato de tabela', (string) => { 
-        the(id) 
-    });
-    And( 'as {string} devem ser separadas por disciplinas');
-});
+Before({ tags: '@noReportData' }, () => {
+    cy.intercept('').as('getClassMeans')
+})
 
-///////////////////////////////////////////////////////////
-Given(`que estou na rota {string}`, (pagina) => {
-    visit(pagina);
-    Then('eu devo ver uma mensagem de {string}', (id) => {
-        the(id).should('contain',"")
-    });
-});
+Given('que eu estou na rota "{route}"', route => {
+    cy.visit(route)
+})
 
+Then('eu deveria ver um aviso de "não tenho acesso à página"', () => {
+    cy.wait(1000)
+    cy.get('[data-test-id=loginPage]').should('be.enabled')
+})
+
+Then('devo ver uma mensagem de "não há dados aqui ainda"', () => {
+    cy.wait('@getClassMeans')
+    cy.get('[data-test-id=noGrades]').should('be.enabled')
+})
+
+Then('Então eu deveria ver "as médias das turmas", dispostas em formato de tabela', () => {
+    cy.intercept('').as('getClassMeans')
+    cy.wait('@getClassMeans')
+    cy.get('[data-test-id=gradeTable]').should('be.enabled')
+})
