099cb802c45c678600ac927e8086d160eb3ac8f2
hagattagi <hagatta.giovanna1@gmail.com>
2022-09-23 00:00:36 -0300

    documentação da classe controller

--- backend/app/controllers/cclasses_controller.rb
+++ backend/app/controllers/cclasses_controller.rb
@@ -1,3 +1,5 @@
+
+
 # Classe Controller que define as ações capazes de serem feitas para manipular um Cclass
 # por meio da aplicação
 
@@ -41,6 +43,10 @@ class CclassesController < ApplicationController
     render json: @cclasses, status: :ok
   end
 
+  ##
+  # Método que renderiza as respostas ao questionário da turma @cclass
+  #
+  # Retorna um json do tipo { rows: [], columns: [], average_rows: [], charts: [] } com status ok (200)
   def get_answers
     set_cclass
 
@@ -54,6 +60,9 @@ class CclassesController < ApplicationController
     render json: @answers, status: :ok
   end
 
+  ##
+  # Método que adiciona as linhas com as médias das questões na variável @answers
+  #
   def set_average_rows
     @answer = {}
     @cclass.survey_answers.first.question_answers
@@ -73,6 +82,9 @@ class CclassesController < ApplicationController
     @answers[:average_rows].push(@answer)
   end
 
+  ##
+  # Método que adiciona a variável @answer as respostas médias as perguntas de escala
+  # Recebe como parâmetro +likert_scale_answers+, que é um array de +LikertScaleAnswer+
   def set_likert_scale_average_answers(likert_scale_answers)
     likert_scale_answers.each do |likert_scale_answer|
       likert_scale_question = likert_scale_answer.likert_scale_question
@@ -80,6 +92,9 @@ class CclassesController < ApplicationController
     end
   end
 
+  ##
+  # Método que filtra as respostas a perguntas do tipo likert_scale e as passa para a função set_likert_scale_answers_charts para que sejam adicionados os dados dos gráficos
+  #
   def set_charts
     @cclass.survey_answers.first.question_answers
     .includes(:survey_question, survey_question: [:question_type, :question_answers], likert_scale_answers: [:likert_scale_question])
@@ -88,12 +103,20 @@ class CclassesController < ApplicationController
     end
   end
 
+  ##
+  # Método que itera sobre as respostas de escala e as passas para o método set_likert_scale_answers_chart para adicionar os dados do gráfico de cada pergunta
+  #
+  # Recebe como argumento +likert_scale_answers+, que é um array de +LikertScaleAnswer+
   def set_likert_scale_answers_charts(likert_scale_answers)
     likert_scale_answers.each do |likert_scale_answer|
       set_likert_scale_answers_chart(likert_scale_answer)
       end
   end
 
+  ##
+  # Método que adiciona à variável @answers os dados de um gráfico de uma pergunta do tipo likert_scale
+  #
+  # Recebe como argumento +likert_scale_answer+, que é um objeto do tipo +LikertScaleAnswer+
   def set_likert_scale_answers_chart(likert_scale_answer)
     chart = {
       labels: [],
@@ -109,6 +132,8 @@ class CclassesController < ApplicationController
       @answers[:charts].push(chart)
   end
 
+  ##
+  # Método que adiciona as respostas do questionário na variável @answers
   def set_answers
     @cclass.survey_answers
     .includes(:question_answers, question_answers: [:likert_scale_answers, :survey_question, likert_scale_answers: [:likert_scale_question]])
@@ -117,6 +142,9 @@ class CclassesController < ApplicationController
     end
   end
 
+  ##
+  # Método que adiciona uma linha com as respostas de um aluno ao questionário na variável @answers
+  # Recebe como argumento +survey_answer+, que é um objeto do tipo +SurveyAnswer+
   def set_answer(survey_answer)
     @answer = {}
     survey_answer.question_answers.each do |question_answer|
@@ -130,12 +158,18 @@ class CclassesController < ApplicationController
     @answers[:rows].push(@answer)
   end
 
+  ##
+  # Método que adiciona a variável @answer as respostas as perguntas de escala
+  #
+  # Recebe como parâmetro +likert_scale_answers+, que é um array de +LikertScaleAnswer+
   def set_likert_scale_answers(likert_scale_answers)
     likert_scale_answers.each do |likert_scale_answer|
       @answer[likert_scale_answer.likert_scale_question.question] = likert_scale_answer.answer
     end
   end
 
+  ##
+  # Método que adiciona as perguntas do questionário na variável @answers
   def set_questions
     @cclass.survey_answers.first.question_answers.each do |question_answer|
       survey_question = question_answer.survey_question
@@ -147,17 +181,27 @@ class CclassesController < ApplicationController
     end
   end
 
+  ##
+  # Método que adiciona as perguntas de escala do questionário na variável @answers
+  #
+  # Recebe como argumento +likert_scale_questions+, que é um array de +LikertScaleQuestion+
   def set_likert_scale_questions(likert_scale_questions)
     likert_scale_questions.each do |likert_scale_question|
       @answers[:columns].push(likert_scale_question.question)
     end
   end
 
+  ##
+  # Método que renderiza os dados da turma cujo id é passado como parametro na requisição
+  #
+  # Retorna um json com os dados da turma
   def show
     @cclass = Cclass.find(params[:id])
     render json: @cclass, status: :ok
   end
 
+  ##
+  # Método que atribui a variável @cclass a turma cujo id é passado como parametro na requisição
   def set_cclass
     @cclass = Cclass.find(params[:id])
   end
