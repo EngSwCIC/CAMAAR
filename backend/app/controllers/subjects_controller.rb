56e01c0fd7811f30253730700e4969db935282b8
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2022-09-22 23:16:35 -0300

    Revert "documentacao de subject"
    This reverts commit 0c13aa2ce081a141267c14c9feb1426c63751f4d.

--- backend/app/controllers/subjects_controller.rb
+++ backend/app/controllers/subjects_controller.rb
@@ -1,16 +1,10 @@
 class SubjectsController < ApplicationController
   def index
-    ##
-    # Retrona todos os dados do banco de dados
-
     @subjects = Subject.all
     render json: @subjects, status: :ok
   end
 
   def show
-    ##
-    # Retorna apenas o dado de id :id
-
     @subject = Subject.find(params[:id])
     render json: @subject, status: :ok
   end
