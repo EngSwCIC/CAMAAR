fd808a16a4cc4023000bac5e637eb89df356adce
iiiqwertyiii <nicpabe@gmail.com>
2022-09-22 22:11:14 -0300

    documentação rdoc

--- backend/app/controllers/subjects_controller.rb
+++ backend/app/controllers/subjects_controller.rb
@@ -9,7 +9,18 @@ class SubjectsController < ApplicationController
     render json: @subject, status: :ok
   end
 
-  def admin_report 
+  ##
+  # Importa todas as matérias (subjects) do banco de dados e guarda em um array de objetos.
+  #
+  # Em subjects, há iteração para a criação de um novo array,
+  # com cada matéria desconstruída em código (name) e classes (classes).
+  #
+  # Para cada subject, coleta-se as informações de cada classe, com a média das notas (grade)
+  # e o código da classe (name).
+  #
+  # Retorna um JSON de disciplinas e suas informações, como nome e turmas.
+
+  def admin_report
     subjects = Subject.all
 
     subjects = subjects.map { |s| { name: s.code, classes: s.cclasses } }
@@ -24,11 +35,18 @@ class SubjectsController < ApplicationController
         end
       }
     end
-    
+
     render json: subjects
   end
 
   private
+
+  ##
+  # Calcula a média das turmas.
+  #
+  # Retorna a média se houver nota,
+  # caso contrário, retorna "0".
+
   def get_class_mean(oi)
     a = oi.enrollments.map { |b| b.grade }
     return a.inject(0) { |sum, x| sum + x.to_i } / a.size unless a.size == 0
