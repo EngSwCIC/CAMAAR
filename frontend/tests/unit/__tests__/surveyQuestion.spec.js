19cb5dafaa8eaaff50f1fb8ffe11f4282ff9f9f3
thiagorigstak <thiagorigstak@gmail.com>
2023-01-19 20:26:05 -0300

    feat. Testes para mutipla escolha

--- frontend/tests/unit/__tests__/surveyQuestion.spec.js
+++ frontend/tests/unit/__tests__/surveyQuestion.spec.js
@@ -1,6 +1,8 @@
 describe("Survey Question Tests", () => {
+
+  it.todo("Componente deve existir");
+
   describe("Checkbox Tests", () => {
-    it.todo("Componente deve existir");
 
     it.todo(
       "Componente deve haver a possibilidade de adicionar caixa de seleção independente"
@@ -11,5 +13,18 @@ describe("Survey Question Tests", () => {
     it.todo("Componente consegue alternar estado da caixa de seleção");
 
     it.todo("Componente deve conseguir nomear caixa de seleção");
+  });
+
+  describe("RadioInput Tests", () =>{
+
+    it.todo("Componente deve permitir adicionar uma escolha");
+    
+    it.todo("Componente deve permitir a remoção de uma escolha");
+
+    it.todo("Componente deve ser capaz de selecionar apenas uma escolha");
+
+    it.todo("Componente deve ser capaz de nomear as diferentes escolhas");
+
+    it.todo("Componente deve permitir a alternação entre as escolhas")
   });
 });
