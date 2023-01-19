d89f995ba7b7536896574ceabd4d113698a884c0
LucasVeloz <lurucell@gmail.com>
2023-01-19 20:16:08 -0300

    feat: adding todo tests of checkbox in surveyQuestion

---
+++ frontend/tests/unit/__tests__/surveyQuestion.spec.js
@@ -0,0 +1,13 @@
+describe("Survey Question Tests", () => {
+  it.todo("Componente deve existir");
+
+  it.todo(
+    "Componente deve haver a possibilidade de adicionar caixa de seleção independente"
+  );
+
+  it.todo("Componente deve conseguir remover caixa de seleção");
+
+  it.todo("Componente consegue alternar estado da caixa de seleção");
+
+  it.todo("Componente deve conseguir nomear caixa de seleção");
+});
