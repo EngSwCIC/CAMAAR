750f89c330617112a4a47f4c306d27640d1483f3
kleberjr <kleb3r.jr@gmail.com>
2023-01-29 12:45:02 -0300

    feat: setting up mailer

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  def redefine_password(user)
+	@redefinition_link = 'user.redefinition_link'
+	@user_name = 'user.name'
+	# Needs to be a valid email address
+	# Replace by yours to test
+	@user_email = 'user.email'
+
+    mail(to: @user_email, subject: "Sistema CAMAAR: Definição de Senha")
+  end
 end
--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -34,9 +34,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
+	port: 587,
+	authentication: "plain",
+	enable_starttls_auto: true,
+	user_name: ENV["GMAIL_USERNAME"],
+	password: ENV["GMAIL_PASSWORD"]
+  }
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
