4c47acc322e41cd466afc1b1f5ceac879c3d0bef
weliton <welitoncpb@gmail.com>
2023-01-29 16:26:44 -0300

    Adicionando arquivo Questionarios.js

---
+++ frontend/tests/e2e/integration/Questionarios/Questionarios.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-undef */
+import { Given, Then, And, Before } from "cypress-cucumber-preprocessor/steps";
+const { get, visit, the, wait, intercept, clearLocalStorageCache} = cy;
