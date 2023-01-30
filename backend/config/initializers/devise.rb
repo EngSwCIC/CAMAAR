5f3a55e65d7dfaf345a699031141f0bb74e19938
maldisco <filipe.sf04@gmail.com>
2023-01-30 10:16:34 -0300

    undo jwt.secret

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -16,8 +16,7 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'c5d5856b6b233bfe95a9f51deef47b8903cbdfeea669396837225867c49c258dcda714aa6bf61dfca284492f244b0ffdfc457cbe1b940b52545de5aa1ffcfba0'
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
-    # jwt.secret = ENV['JWT_SECRET_KEY']
+    jwt.secret = ENV['JWT_SECRET_KEY']
   end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
