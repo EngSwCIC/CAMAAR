be4459fd8b2d433582b80c0d655c3f2c49c0fa2a
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2023-01-30 23:25:01 -0300

    adicionando teste do cenário 5, possivel de refatoracao

--- frontend/tests/e2e/integration/Questionarios/Questionarios.js
+++ frontend/tests/e2e/integration/Questionarios/Questionarios.js
@@ -73,4 +73,45 @@ Then(`o textarea da questão {} deveria enviar {}`, (num_questao, conteudo_testa
 
 });
 
+// Given(/que estou no questionario "(\w+)"/, (string) =>{
+//   visit("/#/surveys/" + string)
+// })
+// When(`preencho o textarea da questão "${numero}" com "${string}"`, (numero, string) => {
+//   cy.get(`#questao-${numero} textarea`).type(string)
+// });
+//
+// And (`preencho o radio button da questão "${numero}" com os labels: "${labels}"`, (numero ,labels) => {
+//   labels.split(/,\s?/).forEach(label =>
+//     cy.get(`#questao-${numero} .q-radio__label label`).contains(label).click()
+//   )
+// });
+// And(`clico no botão "${button_content}"`, (button_content) => {
+//   cy.get('.q-btn').contains(button_content).click()
+//   cy.wait('@getRequest').then(interception => {
+//     cy.wrap(interception.request.body.answers_attributes).as('questoes_enviadas')
+//   })
+// });
+//
+// Then(`devo ver uma mensagem de "${string}"`, (string) => {
+//   get('.q-notification__message').should('contain', string)
+// });
+//
+// And(`o textarea da questão "${num_questao}" deveria enviar "${conteudo_testado}"`, (num_questao, conteudo_testado) => {
+//   let indice = parseInt(num_questao) - 1
+//   cy.get('@questoes_enviadas').then(questao => {
+//     expect(questao[indice].content).to.equal(conteudo_testado)
+//   });
+// });
+//
+// And(`o campo "radioButton" opções deveria enviar: "${labels}"`, (labels) => {
+//   let expectedLabels = labels.split(', ');
+//   cy.get('@questoes_enviadas').then(questao => {
+//     questao.forEach(questaoEnviada => {
+//       if (questaoEnviada.question_id === 'radioButton') {
+//         let labelsEnviados = questaoEnviada.options.split(', ');
+//         expect(labelsEnviados).to.deep.equal(expectedLabels);
+//       }
+//     });
+//   });
+// });
 
