294f38704cd098faaad1ccb14ee01317c1c80b7d
vnss-costa <vanessa.costa@bpoconsig.com.br>
2023-02-03 19:11:21 -0300

    configura o factory bot

---
+++ backend/spec/factories/surveys.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :survey do
+    id {1}
+    answer {"Muito boa essa disciplina"}
+    survey_answer_id {1}
+    survey_question_id {1}
+    created_at {"2023-01-30T14:03:40.689Z"} 
+    updated_at {"2023-01-30T14:03:40.689Z"}
+  end
+end
