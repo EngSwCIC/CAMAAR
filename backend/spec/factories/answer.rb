7809d2767e826e1350d6792a07d13870046a1216
vnss-costa <vanessa.costa@bpoconsig.com.br>
2023-02-03 18:07:57 -0300

    adiona factory_bot e inicia os testes

---
+++ backend/spec/factories/answer.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do 
+    factory :answer do
+        id {'0105453'}
+        answer {"Disciplina muito interessante"}
+        survey_answer_id {1}
+        survey_question_id {1}
+        created_at {"2023-01-30T14:03:40.689Z"}
+        updated_at {"2023-01-30T14:03:40.689Z"}
+    end
+end
