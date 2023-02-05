2ea718b85e46ef64713c0ec3207a790806dc847c
Gabriel Nogueira <gab.nog94@gmail.com>
2023-02-05 00:13:25 -0300

    Fix unit test for loinSigaa

--- backend/spec/services/application_service_spec.rb
+++ backend/spec/services/application_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ApplicationService, type: :model do
   describe '#loginSigaa' do
     it 'Checks if it can log in to the SIGAA page' do
       browser = SigaaManager::ClassCollector.loginSigaa
-      puts(browser.element(tag_name:'div', id:"turmas-portal", class:"simple-panel").exists?)
+      expect((browser.element(tag_name:'div', id:"turmas-portal", class:"simple-panel").exists?)).to be_truthy
     end
   end
 end
