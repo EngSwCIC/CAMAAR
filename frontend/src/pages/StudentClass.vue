8930bccf03beb2d6fcd35461e842ea53bae31470
vitorino99 <vitorcamilolemos@gmail.com>
2023-02-10 18:56:31 -0300

    Refatoração da media de avaliações de forma correta (média e moda)

--- frontend/src/pages/StudentClass.vue
+++ frontend/src/pages/StudentClass.vue
@@ -1,12 +1,15 @@
+
 <template>
-  <div class="q-pa-md">
-    <q-table
-        title="Média das avaliações"
-        :rows="rowsAverage"
-        :columns="columnsAverage"
-        row-key="name"
-    />
-  </div>
+  <h4>Media das avaliações</h4>
+  <table class="table-bordered">
+    <tr>
+      <th v-for="(column, index) in columns" :key="index">{{ column }}</th>
+    </tr>
+    <tr v-for="(row, key) in filteredAverage" :key="key">
+      <td>{{ key }}</td>
+      <td v-for="(value, index) in row" :key="index">{{ value }}</td>
+    </tr>
+  </table>
   <h4>Avaliação dos Estudantes</h4>
   <table class="table-bordered">
     <tr>
@@ -25,7 +28,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      filteredAnswers: [],
+      filteredAverage: [],
       questionAnswers: [],
       resposta: null,
       columns: [],
@@ -53,11 +56,53 @@ export default {
       }
       acc[curr.survey_question_id].push(curr.answer);
       this.resposta = acc
+      this.filteredResult();
       return acc;
     }, {});
+  },
+  filteredResult() {
+      let filteredResult = {};
+
+      for (const key in this.resposta) {
+        if (Array.isArray(this.resposta[key])) {
+          let values = this.resposta[key];
+
+          if (this.isNumeric(values[0])) {
+            let sum = values.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+            let avg = sum / values.length;
+            filteredResult[key] = [parseFloat(avg).toFixed(2).toString()];
+          } else if (typeof values[0] === "string") {
+            let frequency = {};
+            let maxFrequency = 0;
+            let mode = [];
+
+            values.forEach(value => {
+              if (frequency[value]) {
+                frequency[value]++;
+              } else {
+                frequency[value] = 1;
+              }
+
+              if (frequency[value] > maxFrequency) {
+                maxFrequency = frequency[value];
+                mode = [value];
+              } else if (frequency[value] === maxFrequency) {
+                mode.push(value);
+              }
+            });
+
+            filteredResult[key] = mode;
+          }
+        }
+      }
+      console.log(filteredResult)
+      this.filteredAverage = filteredResult
+      return filteredResult;
+    },
+    
+    isNumeric(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
   }
-
-
   }
 };
 
