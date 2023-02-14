e5779ac9878a4a9e77bc0c2b538523812fe7b01b
Ruan Petrus Alves Leite <ruan@pop-os.localdomain>
2023-02-14 19:09:36 -0300

    Resolvi os problemas com os testes dos membros, criei factories para membros e roles, e adicionei os testes sem depender do banco de dados para link valido invalido e sem link

--- backend/spec/controllers/members_controller.rb
+++ backend/spec/controllers/members_controller.rb
@@ -2,36 +2,64 @@ require 'rails_helper'
 require 'factory_bot_rails'
 
 RSpec.describe MembersController, type: :controller do
+  include FactoryBot::Syntax::Methods
+
   describe 'GET #redefine_password' do
     context 'when the redefinition_link is valid' do
-      # This member has to be created to this test to work
-      role_discente = Role.create(name: 'discente', description: 'discente')
-      member = Member.new(
-        email: 'email@correto',
-        name: 'name',
-        course: 'course',
-        registration: '123',
-        username: '123',
-        degree: 'graduação',
-        role: role_discente,
-        redefinition_link: 'link_correto_de_redefinicao_de_senha'
-      )
+      let(:members_class) { class_double(Member).as_stubbed_const }
 
       before do
+        @good_member = create(:member)
+
         get :redefine_password, params: { redefinition_link: 'link_correto_de_redefinicao_de_senha' }
       end
 
       it 'responds with a JSON object indicating success' do
+        allow(members_class).to receive(:where).with(redefinition_link: 'link_correto_de_redefinicao_de_senha').and_return(@good_member)
         expect(response).to have_http_status(:ok)
-        expect_body = JSON.parse(response.body)
+      end
 
-        expect(expect_body['name']).to eq(member.name)
-        expect(expect_body['email']).to eq(member.email)
-        expect(expect_body['course']).to eq(member.course)
-        expect(expect_body['registration']).to eq(member.registration)
-        expect(expect_body['username']).to eq(member.username)
-        expect(expect_body['degree']).to eq(member.degree)
-        expect(expect_body['redefinition_link']).to eq(member.redefinition_link)
+      it 'responds the right member fields' do
+        allow(members_class).to receive(:where)
+          .with(redefinition_link: 'link_correto_de_redefinicao_de_senha')
+          .and_return(@good_member)
+        expect_body = JSON.parse(response.body)
+        expect(expect_body).to eq(
+          JSON.parse(@good_member.to_json)
+        )
+      end
+    end
+
+    context 'when the redefinition_link is not valid' do
+      let(:members_class) { class_double(Member).as_stubbed_const }
+
+      before do
+        get :redefine_password, params: { redefinition_link: 'link_errado_de_redefinicao_de_senha' }
+      end
+
+      it 'responds with a JSON object indicating success' do
+        allow(members_class).to receive(:where)
+          .with(redefinition_link: 'link_errado_de_redefinicao_de_senha')
+          .and_return([])
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'responds with nil body' do
+        allow(members_class).to receive(:where)
+          .with(redefinition_link: 'link_errado_de_redefinicao_de_senha')
+          .and_return([])
+        expect_body = JSON.parse(response.body)
+        expect(expect_body).to eq(nil)
+      end
+    end
+
+    context 'With no redefinition link parameter' do
+      it 'Expects 404' do
+        expect(get: '/members/redefine_password/').to route_to(
+          controller: 'members',
+          action: 'show',
+          id: "redefine_password"
+        )
       end
     end
   end
