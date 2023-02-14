bebc21faba8b34b6d5418dde6737f3f10ad70d00
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-02-14 09:41:41 -0300

    adiciona mock no teste do controller

--- backend/spec/controllers/surveys_controller_spec.rb
+++ backend/spec/controllers/surveys_controller_spec.rb
@@ -1,54 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe SurveysController, type: :request do
-  before(:all) do
-    Role.create(name: 'teste', description: 'teste') # This will have id 1
-  end
-
+RSpec.describe SurveysController, type: :controller do
   describe('#create') do
-    describe 'when given valid input' do
-      let(:input)  do
-        {
-          "name": 'Questionário',
-          "description": 'Descrição teste',
-          "expiration_date": '',
-          "semester": '2022/2',
-          "role_id": 1
-        }
-      end
-
-      before(:each) do
-        headers = { 'ACCEPT' => 'application/json' }
-        post '/create_survey', params: { survey: input }, headers: headers
-      end
-
-      it 'creates survey and returns success' do
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to include('2022/2')
-        expect(response.body).to include('Questionário')
-        expect(response.body).to include('Descrição teste')
-      end
-    end
-
-    describe 'when given invalid input' do
-      let(:input)  do
-        {
-          survey: {
-            name: "teste"
-          }
+    it 'creates a new survey and return success' do
+      surver_params = {
+        "name": 'Questionário',
+        "description": 'Descrição teste',
+        "expiration_date": '',
+        "semester": '2022/2',
+        "role_id": 1
       }
-      end
+      survey = Survey.new(surver_params)
 
-      before(:each) do
-        headers = { 'ACCEPT' => 'application/json' }
-        post '/create_survey', params: { survey: input }, headers: headers
-      end
+      allow(Survey).to receive(:new).and_return(survey)
 
-      it 'doesnt creates the survey and returns an error' do
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
+      get :create, params: { survey: surver_params }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(JSON.parse(response.body)).to eq(survey.as_json)
     end
   end
 end
