89453234e8652d246c3fa2fa889881d8940d2ad0
Gabriel Nogueira <gab.nog94@gmail.com>
2023-02-14 14:35:14 -0300

    update scrapper_spec

--- backend/spec/controllers/scrapper_spec.rb
+++ backend/spec/controllers/scrapper_spec.rb
@@ -3,96 +3,96 @@ require 'rails_helper'
 
 RSpec.describe ScrapperController, :type => :controller do
 
-    context 'method show' do
+  context 'method show' do
 
-      it 'should render all the classes' do
+    it 'should render all the classes' do
 
-          get :show
-          expect(response).to have_http_status(200)
-          
-          turmasSigaa = JSON.parse(response.body)['classes']
-          expect(turmasSigaa).to be_an(Array)
-          expect(turmasSigaa.first).to have_key('name')
-          expect(turmasSigaa.first).to have_key('code')
-          expect(turmasSigaa.first).to have_key('class')
-          expect(turmasSigaa.first['class']).to have_key('classCode')
-          expect(turmasSigaa.first['class']).to have_key('semester')
-          expect(turmasSigaa.first['class']).to have_key('time')
+      get :show
+      expect(response).to have_http_status(200)
 
-      end
-
-      it 'should not return a empty response' do
-        get :show
-        expect(response).to have_http_status(200)
-        expect(response.body).not_to be nil
-      end
+      turmasSigaa = JSON.parse(response.body)['classes']
+      expect(turmasSigaa).to be_an(Array)
+      expect(turmasSigaa.first).to have_key('name')
+      expect(turmasSigaa.first).to have_key('code')
+      expect(turmasSigaa.first).to have_key('class')
+      expect(turmasSigaa.first['class']).to have_key('classCode')
+      expect(turmasSigaa.first['class']).to have_key('semester')
+      expect(turmasSigaa.first['class']).to have_key('time')
 
     end
 
-    context "method index" do
+    it 'should not return a empty response' do
+      get :show
+      expect(response).to have_http_status(200)
+      expect(response.body).not_to be nil
+    end
+
+  end
+
+  context "method index" do
 
 
-      it "shoud import all members", skip: true do
+    it "shoud import all members", skip: true do
 
-        membros = [
+      membros = [
+        {
+          code: "CIC0105",
+          classCode: "TA",
+
+          dicente: 
+          [{
+            nome: "Ana Clara Jordao Perna",
+            curso: "CIÊNCIA DA COMPUTAÇÃO/CIC",
+            matricula: "190084006",
+            usuario: "190084006",
+            formacao: "graduando",
+            ocupacao: "dicente",
+            email: "acjpjvjp@gmail.com"
+          },
           {
-            code: "CIC0105",
-            classCode: "TA",
-            
-            dicente: 
-              [{
-                 nome: "Ana Clara Jordao Perna",
-                 curso: "CIÊNCIA DA COMPUTAÇÃO/CIC",
-                 matricula: "190084006",
-                 usuario: "190084006",
-                 formacao: "graduando",
-                 ocupacao: "dicente",
-                 email: "acjpjvjp@gmail.com"
-               },
-               {
-                 nome: "Ana Paula Oliveira da Nobrega Costa",
-                 curso: "ENGENHARIA DE COMPUTAÇÃO/CIC",
-                 matricula: "190142120",
-                 usuario: "190142120",
-                 formacao: "graduando",
-                 ocupacao: "dicente",
-                 email: "anapaulaoncosta@gmail.com"
-               }],
+            nome: "Ana Paula Oliveira da Nobrega Costa",
+            curso: "ENGENHARIA DE COMPUTAÇÃO/CIC",
+            matricula: "190142120",
+            usuario: "190142120",
+            formacao: "graduando",
+            ocupacao: "dicente",
+            email: "anapaulaoncosta@gmail.com"
+          }],
 
-            docente: {
-              nome: "GENAINA NUNES RODRIGUES",
-              departamento: "DEPTO CIÊNCIAS DA COMPUTAÇÃO",
-              formacao: "DOUTORADO",
-              usuario: "78334926120",
-              email: "genaina@unb.br",
-              ocupacao: "docente"
-            }
+          docente: {
+            nome: "GENAINA NUNES RODRIGUES",
+            departamento: "DEPTO CIÊNCIAS DA COMPUTAÇÃO",
+            formacao: "DOUTORADO",
+            usuario: "78334926120",
+            email: "genaina@unb.br",
+            ocupacao: "docente"
           }
-        ]
-        
-        corpo =  {
-          classes:
-            [
-              {:codigo => "CIC0105", :turma => "TA", :nome => "ENGENHARIA DE SOFTWARE", :semestre => "2021.2", :horario => "35M12"}
-            ]
         }
-        
-        
-        # Stub 
-        allow(SigaaManager::ClassExtractor).to receive(:call) do
-          membros.to_json
-        end
-        
-        #post '/import/turmas', params: corpo
-        #headers = {​​​ 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }​​​
-        post :index, params:corpo, body:corpo.to_json as: :json
-        expect(Member.first).to_not be_nil
-        expect(response).to have_http_status(200)
-        expect(response.body).to eq("{\"message\":\"Participantes importados com sucesso\"}")
-        
-      end
-    end
+      ]
 
-    
+      corpo =  {
+        classes:
+        [
+          {:codigo => "CIC0105", :turma => "TA", :nome => "ENGENHARIA DE SOFTWARE", :semestre => "2021.2", :horario => "35M12"}
+        ]
+      }
+
+
+      # Stub 
+      allow(SigaaManager::ClassExtractor).to receive(:call) do
+        membros.to_json
+      end
+
+      #post '/import/turmas', params: corpo
+      #headers = {​​​ 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }​​​
+      post :index, params:corpo, body:corpo.to_json, as: :json
+      expect(Member.first).to_not be_nil
+      expect(response).to have_http_status(200)
+      expect(response.body).to eq("{\"message\":\"Participantes importados com sucesso\"}")
+
+    end
+  end
+
+
 end
 
