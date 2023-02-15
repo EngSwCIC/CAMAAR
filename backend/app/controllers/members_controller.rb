781c10ef72157b309b790d02b2b9e263c6363854
maldisco <filipe.sf04@gmail.com>
2023-02-15 11:40:14 -0300

    bugfix on members_controller and sessions_controller

--- backend/app/controllers/members_controller.rb
+++ backend/app/controllers/members_controller.rb
@@ -20,7 +20,7 @@ class MembersController < ApplicationController
   # #
   # Procura por um membro específico baseado no seu hash de redefinição.
   def redefine_password
-    @member = Member.where(redefinition_has: params[:redefinition_link])[0]
+    @member = Member.where(redefinition_link: params[:redefinition_link])[0]
     render json: @member, status: :ok
   end
 end
