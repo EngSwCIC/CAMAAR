f4d62656b19b3d0447f3c3537e79da0f58b58ff4
vnss-costa <vanessa.costa@bpoconsig.com.br>
2023-02-14 23:36:21 -0300

    feat: cria teste para objeto retornando todas as respostas

--- backend/spec/controllers/surveys_controller_spec.rb
+++ backend/spec/controllers/surveys_controller_spec.rb
@@ -27,23 +27,53 @@ require 'rails_helper'
       it "returns http ok" do
         @r = create(:role)
         @survey = create(:survey)
-        get "/surveys/1/answers"
+        @question_type = create(:question_type)
+        @survey_question = create(:survey_question)
+        @member = create(:member)
+        @cclass = create(:cclass)
+        @survey_answer = create(:survey_answer)
+        get "/surveys/#{@survey.id}/answers"
         expect(response).to have_http_status(:ok)
       end
-
-      it "returns http ok 1" do
+      
+      it "returns an empty object" do
         @r = create(:role)
         @survey = create(:survey)
         @question_type = create(:question_type)
         @survey_question = create(:survey_question)
-        @subject = create(:subject)
         @member = create(:member)
         @cclass = create(:cclass)
         @survey_answer = create(:survey_answer)
-        puts @survey.id
         get "/surveys/#{@survey.id}/answers"
         data = JSON.parse(response.body)
         expect(data.length()).to equal(0)
       end
+
+      it "returns a complete survey" do
+        @r = create(:role)
+        @survey = create(:survey)
+        @question_type = create(:question_type)
+        @survey_question = create(:survey_question)
+        @member = create(:member)
+        @cclass = create(:cclass)
+        @survey_answer = create(:survey_answer)
+        get "/surveys/#{@survey.id}/answers"
+        data = JSON.parse(response.body)
+
+        keys_expected = ['id', 'answer', 'survey_answer_id', 'survey_question_id', 'created_at', 'updated_at']
+
+        missing_key = false
+
+        data.each do |obj|
+          keys_expected.each do |key|
+            unless obj.has_key?(key)
+              missing_key = true
+              break
+            end
+          end
+        end
+        expect(missing_key).to equal(false)
+      end
     end
-  end 
+  end
+
--- backend/spec/factories/survey_answers.rb
+++ backend/spec/factories/survey_answers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :survey_answer do
     survey_id {1}
-    member_id {2}
+    member_id {1}
     cclass_id {1}
   end
 end
