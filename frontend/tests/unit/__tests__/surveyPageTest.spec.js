9ad9b036a4298e7dff4a52552f1b86c759acb900
Barbosa885 <gustavosports7@gmail.com>
2023-02-16 08:01:47 -0300

    refactor: Refatora testes da página de respostas da survey

--- frontend/tests/unit/__tests__/surveyPageTest.spec.js
+++ frontend/tests/unit/__tests__/surveyPageTest.spec.js
@@ -1,28 +1,72 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import axios from "axios";
-import TableComponent from "../../../src/components/TableComponent";
+import StudentClass from "../../../src/pages/StudentClass";
 
-jest.mock("axios", () => ({
-  get: jest.fn().mockResolvedValue({ data }),
-}));
+jest.mock("axios");
 
-describe("Table.vue", () => {
+describe("StudentClass", () => {
   let wrapper;
 
+  const mockData = {
+    data: [
+      {
+        id: 1,
+        survey_question_id: 1,
+        answer: 4,
+      },
+      {
+        id: 2,
+        survey_question_id: 2,
+        answer: 3,
+      },
+      {
+        id: 3,
+        survey_question_id: 1,
+        answer: 2,
+      },
+      {
+        id: 4,
+        survey_question_id: 2,
+        answer: "Bom",
+      },
+    ],
+  };
+
+  beforeAll(() => {
+    axios.get.mockResolvedValue(mockData);
+  });
+
   beforeEach(() => {
-    wrapper = shallowMount(TableComponent, {
-      data() {
-        return {
-          filteredAnswers: [],
-          questionAnswers: [],
-          resposta: null,
-          columns: [],
-        };
+    wrapper = mount(StudentClass, {
+      mocks: {
+        $route: {
+          params: {
+            id: 1,
+          },
+        },
       },
     });
   });
 
-  it("calls getAnswers when created", () => {
-    expect(axios.get).toHaveBeenCalledWith("api/surveys/1/answers");
+  it("should render two tables", () => {
+    expect(wrapper.findAll("table").length).toBe(2);
+  });
+
+  it("should check if a value is numeric", () => {
+    expect(wrapper.vm.isNumeric(4)).toBe(true);
+    expect(wrapper.vm.isNumeric("4")).toBe(true);
+    expect(wrapper.vm.isNumeric("foo")).toBe(false);
+  });
+
+  it("should calculate the average of numeric values in the resposta data", () => {
+    const numericData = {
+      1: [4, 2],
+      2: [3],
+    };
+    wrapper.setData({ resposta: numericData });
+
+    const filteredAverage = wrapper.vm.filteredResult();
+    expect(filteredAverage["1"][0]).toEqual("3.00");
+    expect(filteredAverage["2"][0]).toEqual("3.00");
   });
 });
