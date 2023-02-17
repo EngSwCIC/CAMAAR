768cd0794439400531e91ab026c841983e870b6c
Mateus Lucio <luciomateus100@hotmail.com>
2023-02-17 10:22:26 -0300

    Adiciona nova confuguração do cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  fixturesFolder: 'tests/e2e/fixtures',
+  screenshotsFolder: 'tests/e2e/screenshots',
+  videosFolder: 'tests/e2e/videos',
+  downloadsFolder: 'tests/e2e/downloads',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./tests/e2e/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'tests/e2e/integration/**/*.feature',
+    supportFile: 'tests/e2e/support/index.js',
+  },
+})
