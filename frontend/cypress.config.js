8ceb4110f0ca936ffcaa4f6adb483ba09d1c15d6
Mateus Lucio <luciomateus100@hotmail.com>
2023-02-17 15:28:26 -0300

    Teste e2e Co-authored-by: Lucas Borges <lborges343@gmail.com>

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,19 +1,19 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  fixturesFolder: 'tests/e2e/fixtures',
-  screenshotsFolder: 'tests/e2e/screenshots',
-  videosFolder: 'tests/e2e/videos',
-  downloadsFolder: 'tests/e2e/downloads',
+  fixturesFolder: "tests/e2e/fixtures",
+  screenshotsFolder: "tests/e2e/screenshots",
+  videosFolder: "tests/e2e/videos",
+  downloadsFolder: "tests/e2e/downloads",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./tests/e2e/plugins/index.js')(on, config)
+      return require("./tests/e2e/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:8080',
-    specPattern: 'tests/e2e/integration/**/*.feature',
-    supportFile: 'tests/e2e/support/index.js',
+    baseUrl: "http://localhost:8080/#/create-survey",
+    specPattern: "tests/e2e/integration/CriacaoDeQuestionario/**/*.cy.js",
+    supportFile: "tests/e2e/support/index.js",
   },
-})
+});
