083d7e5f9199acd6ce7c3cb35c55b952da511baa
Mateus Lucio <luciomateus100@hotmail.com>
2023-02-17 23:48:56 -0300

    Adiciona melhorias nos teste e2e
    Co-authored-by: Lucas Sousa Borges <Lucsborges@users.noreply.github.com>
    Co-authored-by: natalylacerda <natalylacerda@users.noreply.github.com>

--- frontend/tests/e2e/integration/CriacaoDeQuestionario/CriacaoDeQuestionario.js
+++ frontend/tests/e2e/integration/CriacaoDeQuestionario/CriacaoDeQuestionario.js
@@ -17,11 +17,35 @@ describe("Questionario", () => {
         .should("have.value", "Questionário");
     });
 
-    it("Cria uma nova pergunta", () => {
+    it("Cria uma nova pergunta (Caixa de Seleção)", () => {
       cy.get('[name="add-question"]').click();
       cy.get("[name=question-title]").should("have.value", "Pergunta 1");
 
       cy.get("[name=question-type]").should("have.value", "Caixa de Seleção");
+    });
+
+    it("Seleciona o tipo de pergunta (texto)", () => {
+      cy.get('[name="add-question"]').click();
+      cy.get("[name=question-title]").should("have.value", "Pergunta 1");
+      cy.contains("Caixa de Seleção").click();
+      cy.contains("Texto").click();
+      cy.get("[name=question-type]").should("have.value", "Texto");
+    });
+
+    it("Seleciona o tipo de pergunta (Múltipla Escolha)", () => {
+      cy.get('[name="add-question"]').click();
+      cy.get("[name=question-title]").should("have.value", "Pergunta 1");
+      cy.contains("Caixa de Seleção").click();
+      cy.contains("Múltipla Escolha").click();
+      cy.get("[name=question-type]").should("have.value", "Múltipla Escolha");
+    });
+
+    it("Seleciona o tipo de pergunta (Escala Linear)", () => {
+      cy.get('[name="add-question"]').click();
+      cy.get("[name=question-title]").should("have.value", "Pergunta 1");
+      cy.contains("Caixa de Seleção").click();
+      cy.contains("Escala Linear").click();
+      cy.get("[name=question-type]").should("have.value", "Escala Linear");
     });
 
     it("Deleta pergunta", () => {
