6c279d78d80be0fe2c79889870566afa234d0069
Barbosa885 <gustavosports7@gmail.com>
2023-02-18 02:40:32 -0300

    feat: cria teste da página StudentClass

--- frontend/tests/unit/__tests__/surveyPageTest.spec.js
+++ frontend/tests/unit/__tests__/surveyPageTest.spec.js
@@ -1,32 +1,72 @@
 import { mount } from "@vue/test-utils";
 import axios from "axios";
-import StudentClass from "@/components/StudentClass.vue";
+import StudentClass from "../../../src/pages/StudentClass";
+
+jest.mock("axios");
 
 describe("StudentClass", () => {
   let wrapper;
 
+  const mockData = {
+    data: [
+      {
+        id: 1,
+        survey_question_id: 1,
+        answer: 4,
+      },
+      {
+        id: 2,
+        survey_question_id: 2,
+        answer: 3,
+      },
+      {
+        id: 3,
+        survey_question_id: 1,
+        answer: 2,
+      },
+      {
+        id: 4,
+        survey_question_id: 2,
+        answer: "Bom",
+      },
+    ],
+  };
+
   beforeEach(() => {
-    const id = 1;
-    const url = "https://api.example.com";
-    const response = await axios.get(url);
-    const mockData = { data: response.data };
+    axios.get.mockResolvedValue(mockData);
+  });
+
+  beforeEach(() => {
     wrapper = mount(StudentClass, {
       mocks: {
         $route: {
           params: {
-            id,
+            id: 1,
           },
         },
       },
-      data() {
-        return {
-          resposta: mockData.data,
-        };
-      },
-    });
-    it("should render two tables", () => {
-      expect(wrapper.findAll("table").length).toBe(2);
     });
   });
-});
 
+  it("should render two tables", () => {
+    expect(wrapper.findAll("table").length).toBe(2);
+  });
+
+  it("should check if a value is numeric", () => {
+    expect(wrapper.vm.isNumeric(4)).toBe(true);
+    expect(wrapper.vm.isNumeric("4")).toBe(true);
+    expect(wrapper.vm.isNumeric("foo")).toBe(false);
+  });
+
+  it("should calculate the average of numeric values in the resposta data", () => {
+    const numericData = {
+      1: [4, 2],
+      2: [3],
+    };
+    wrapper.setData({ resposta: numericData });
+
+    const filteredAverage = wrapper.vm.filteredResult();
+    expect(filteredAverage["1"][0]).toEqual("3.00");
+    expect(filteredAverage["2"][0]).toEqual("3.00");
+  });
+});
