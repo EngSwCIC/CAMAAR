2d8eabb52674ad397ebda93cc14ae7737f038141
Barbosa885 <gustavosports7@gmail.com>
2023-02-18 16:00:00 -0300

    docs: adiciona comentários ao teste

--- frontend/tests/unit/__tests__/surveyPageTest.spec.js
+++ frontend/tests/unit/__tests__/surveyPageTest.spec.js
@@ -2,12 +2,12 @@ import { mount } from "@vue/test-utils";
 import axios from "axios";
 import StudentClass from "../../../src/pages/StudentClass";
 
-jest.mock("axios");
+jest.mock("axios"); // cria um mock da biblioteca axios
 
 describe("StudentClass", () => {
   let wrapper;
 
-  const mockData = {
+  const mockData = { // define um objeto com dados mock para ser retornado pelo Axios
     data: [
       {
         id: 1,
@@ -32,11 +32,11 @@ describe("StudentClass", () => {
     ],
   };
 
-  beforeEach(() => {
+  beforeEach(() => { // antes de cada teste, configura o mock do Axios para retornar o objeto mockData
     axios.get.mockResolvedValue(mockData);
   });
 
-  beforeEach(() => {
+  beforeEach(() => { // antes de cada teste, cria uma instância da classe "StudentClass" com um objeto mock do Vue.js
     wrapper = mount(StudentClass, {
       mocks: {
         $route: {
@@ -48,16 +48,19 @@ describe("StudentClass", () => {
     });
   });
 
+  // testa se a as duas tabelas são geradas corretamente na página
   it("should render two tables", () => {
     expect(wrapper.findAll("table").length).toBe(2);
   });
 
+  // testa se os valores gerados na tabela são numéricos
   it("should check if a value is numeric", () => {
     expect(wrapper.vm.isNumeric(4)).toBe(true);
     expect(wrapper.vm.isNumeric("4")).toBe(true);
     expect(wrapper.vm.isNumeric("foo")).toBe(false);
   });
 
+  // verifica se o cálculo da média está sendo feito corretamente
   it("should calculate the average of numeric values in the resposta data", () => {
     const numericData = {
       1: [4, 2],
