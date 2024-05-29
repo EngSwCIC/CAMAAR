d8be14333b815bf74a77dbcd6df36c0f51a8934d
jpantonov <jpantonow@gmail.com>
2024-05-29 10:46:52 -0300

    features on auth

--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   get 'home/index'
   # get 'users/page'
   # get 'users/form_student'
-  match '/users/page',   to: 'users#page',   via: 'get'
-  match '/users/forms',   to: 'users#form_student',   via: 'get'
+  match '/users/page', to: 'users#page', via: 'get'
+  match '/users/forms',   to: 'users#form_student', via: 'get'
   # match '/users/page',   to: 'users#page',   via: 'get'
   # match '/users/page',   to: 'users#page',   via: 'get'
   # match '/admins/login', to: 'admins#login', via: 'get'
-  match '/admins/page',   to: 'admins#page',   via: 'get'
-  match '/admins/forms',   to: 'admins#form_student',   via: 'get'
-  match '/admins/dashboards',   to: 'admins#dashboards',   via: 'get'
-  match '/admins/import',   to: 'admins#importdata',   via: 'get'
+  match '/admins/page',   to: 'admins#page', via: 'get'
+  match '/admins/forms', to: 'admins#form_student', via: 'get'
+  match '/admins/dashboards', to: 'admins#dashboards', via: 'get'
+  match '/admins/import', to: 'admins#importdata', via: 'get'
 
   # get 'admins/page'
   # get 'admins/form_student'
@@ -19,37 +19,35 @@ Rails.application.routes.draw do
   # get 'layouts/define_password'
   # devise_for :users
 
-
   devise_for :users, controllers: { registrations: 'users/registrations' },
-  :path => 'users',
-  :path_names => {
-    :sign_in => 'login',
-    :sign_up => 'register',
-    :sign_out => 'logout',
-    :password => 'recover-password',
-    :define => 'define-password',
-    :confirmation => 'verification'
-  }
+                     path: 'users',
+                     path_names: {
+                       sign_in: 'login',
+                       sign_up: 'register',
+                       sign_out: 'logout',
+                       password: 'recover-password',
+                       define: 'define-password',
+                       confirmation: 'verification'
+                     }
 
-  devise_for :admins,
-  :path => 'admins',
-  :path_names => {
-    :sign_in => 'login',
-    :sign_up => 'register',
-    :sign_out => 'logout',
-    :password => 'recover-password',
-    :define => 'define-password',
-    :confirmation => 'verification'
-  }
+  devise_for :admins, controllers: { registrations: 'admins/registrations' },
+                      path: 'admins',
+                      path_names: {
+                        sign_in: 'login',
+                        sign_up: 'register',
+                        sign_out: 'logout',
+                        password: 'recover-password',
+                        define: 'define-password',
+                        confirmation: 'verification'
+                      }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root :to => "home#index"
-
+  root to: 'home#index'
 end
