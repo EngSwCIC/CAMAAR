acb87a295b656f356a075dde08977b52f908c742
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2024-05-29 10:46:31 -0300

    test: Criação de form de avaliação

---
+++ features/step_definitions/CAMAAR_step.rb
@@ -0,0 +1,27 @@
+Given(/^(?:|I )am registered$/) do
+   pending
+end
+
+Given(/^(?:|I )am not registered$/) do
+   pending
+end
+
+Given(/^(?:|I )am an admin$/) do
+   pending
+end
+
+Given(/^(?:|I )am not an admin$/) do
+   pending
+end
+
+Given(/^(?:|I) have an existing template named "(.+)"$/) do |template_name|
+   pending
+end
+
+Given(/^the step I am in is "(.+)"$/) do |management_step|
+   pending
+end
+
+Then(/^I should see the class selection table$/) do
+   pending
+end
