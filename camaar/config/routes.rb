1f7a98224f00e99fe102b178cb1df75fe88730ce
Willyan Marques <willyanmarquesmelo@gmail.com>
2024-05-29 09:52:13 -0300

    reorganização considerando rotas de autenticacao (admins e users)

--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  namespace :admin do
-    #TODO: fazer info do dpto vir do user! estudar devise e session control
-    get "/classes/:department_initials", to: "classes#index", as: :department_classes
-    get "/import", to: "classes#create", as: :import_classes
-    get "/results", to: "answers#index"
-    get "/dispatch", to: "forms#create"
+  get "/classes/:department_initials", to: "classes#index", as: :department_classes
+  get "/import", to: "classes#create", as: :import_classes
+  get "/results", to: "answers#index"
+  get "/dispatch", to: "forms#create"
 
-    resources :templates
-  end
-
-  namespace :user do
-    get "/forms", to: "forms#index"
-    get "/forms/:id", to: "forms#edit"
-  end
+  resources :templates
+  get "/forms", to: "forms#index"
+  get "/forms/:id", to: "forms#edit"
 end
