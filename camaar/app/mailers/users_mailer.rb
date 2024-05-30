8ea6ab0fc039c8339537bc0563fe6d4ad00f929a
jpantonov <jpantonow@gmail.com>
2024-05-30 17:10:11 -0300

    changes on register page

--- camaar/app/mailers/users_mailer.rb
+++ camaar/app/mailers/users_mailer.rb
@@ -1,20 +1,17 @@
 class UsersMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.users_mailer.welcome.subject
   #
   def welcome
-    @greeting = "Hi"
+    @greeting = 'Hi'
 
-    mail to: "to@example.org"
+    mail to: 'to@example.org'
   end
 
   def new_user_email(user)
     @user = user
     mail(to: email, subject: 'New user signup')
   end
-
-  
 end
