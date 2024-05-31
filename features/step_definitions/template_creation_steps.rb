1c861e575b00304856af183449146be23cb3f9d5
Pedro Beneveli <pedro.beneveli@gmail.com>
2024-05-31 16:41:18 -0300

    More specific steps

--- features/step_definitions/template_creation_steps.rb
+++ features/step_definitions/template_creation_steps.rb
@@ -1,28 +1,24 @@
 # tem como fazer ele preencher as coisas so de uma area? Tipo preenche a area da Q2
-When /^I create a question of type "(.*)"$/ do |type|
-  case type
+# o "Big +" seria o botao + externo que coloca mais questoes
+When /^I create a text question with the question "(.*)"$/ do |pergunta|
+  When %{I press "Big +"}
+  And %{I select "Texto" from "Tipo:"}
+  And %{I fill in "Texto:" with "#{pergunta}"}
+end
 
-  when /^Radio$/
-    When %{I press "+"}
-    And %{I select "Radio" from "Tipo"}
-    And %{I fill in "Texto:" with "q do tipo radio"}
-    And %{I fill in "Opções:" with "op1"}
-
-  when /^Texto$/
-    When %{I press "+"}
-    And %{I select "Texto" from "Tipo"}
-    And %{I fill in "Texto:" with "q do tipo texto"}
-
-  else
-    raise "Non-existent question type"
-  end
+# nao sei como eu faria pra 2 alternativas
+When /^I create a radio question with the question "(.*)" and 1 alternative: "(.*)"$/ do |pergunta, alt1|
+  When %{I press "Big +"}
+  And %{I select "Radio" from "Tipo"}
+  And %{I fill in "Texto:" with "#{pergunta}"}
+  And %{I fill in "Opções:" with "#{alt1}"}
 end
 
 Given /^I have created a template "(.*)"$/ do |nome|
   Given %{I am on the Templates page}
   When %{I press "+"}
-  And %{I fill in I fill in "Nome do template:" with "#{nome}"}
-  And %{I create a question of type "Radio"}
-  And %{I create a question of type "Texto"}
+  And %{I fill in "Nome do template:" with "#{nome}"}
+  And %{I create a radio question with the question "radio?" and 1 alternative: "sim"}
+  And %{I create a text question with the question "texto?"}
   And %{I press "Criar"}
 end
