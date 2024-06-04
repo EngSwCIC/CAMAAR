f9489b4e2cee23fcf3dd97194def7ca5edd2ec3c
jpantonov <jpantonow@gmail.com>
2024-06-04 11:14:32 -0300

    register without registering an email

--- camaar/app/mailers/users_mailer.rb
+++ camaar/app/mailers/users_mailer.rb
@@ -1,3 +1,5 @@
+include Rails.application.routes.url_helpers
+
 class UsersMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
