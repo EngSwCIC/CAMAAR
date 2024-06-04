d0b3d9a6372298864053bed177a3174180cb1e44
jpantonov <jpantonow@gmail.com>
2024-06-03 21:33:34 -0300

    protection route

--- camaar/app/controllers/application_controller.rb
+++ camaar/app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!, unless: :allowed_path?
 
   def after_sign_in_path_for(_resource)
-    if admin_signed_in? # Assuming there is such a function
+    if admin_signed_in?
       admins_page_path
     elsif user_signed_in?
       users_page_path
@@ -20,5 +21,40 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: %i[email password]
     devise_parameter_sanitizer.permit :define, keys: [:email]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def allowed_path?
+    allowed_paths = [
+      root_path,
+      new_user_session_path,
+      new_user_password_path,
+      new_admin_session_path,
+      new_admin_password_path
+    ]
+
+    allowed_paths.include?(request.path) ||
+      valid_reset_password_path? ||
+      valid_registration_path? ||
+      valid_confirmation_path?
+  end
+
+  def valid_reset_password_path?
+    request.path == edit_user_password_path && params[:reset_password_token].present?
+  end
+
+  def valid_registration_path?
+    request.path == new_user_registration_path && params[:registration_token].present?
+  end
+
+  def valid_confirmation_path?
+    request.path == user_confirmation_path && params[:confirmation_token].present?
+  end
+
+  def authenticate_user!
+    return if user_signed_in? || admin_signed_in?
+
+    render json: { error: 'Acesso Proibido' }, status: :forbidden
   end
 end
