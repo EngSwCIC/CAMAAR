ea5f5c553c72f75a3c0458c1c238cb0b65863035
jpantonov <jpantonow@gmail.com>
2024-06-06 10:40:41 -0300

    rspec for models

--- camaar/spec/models/user_spec.rb
+++ camaar/spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "é válido com atributos válidos" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "é inválido com email inválido" do
+    user = FactoryBot.build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "é inválido com senha inválida" do
+    user = FactoryBot.build(:user, password: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "não pode haver usuarios com o mesmo email" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user, email: user1.email)
+    expect(user2).to_not be_valid
+  end
+
+  it "os atributos podem se assimilar, mas o email é único" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user, email: "unique@example.com")
+    expect(user2).to be_valid
+  end
 end
