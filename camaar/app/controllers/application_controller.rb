2197fce14668658b10dfbd357f0f398fbb3a51ef
jpantonov <jpantonow@gmail.com>
2024-06-07 14:16:20 -0300

    method for routes protection

--- camaar/app/controllers/application_controller.rb
+++ camaar/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!, unless: :devise_controller?
   # before_action :authenticate_user!, unless: :allowed_path?
   # before_action :redirect_signed_in, only: [:index]
 
@@ -16,22 +17,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def allowed_path?
-    allowed_paths = [
-      root_path,
-      new_user_session_path,
-      new_user_password_path,
-      new_admin_session_path,
-      new_admin_password_path
-    ]
-
-    allowed_paths.include?(request.path) ||
-      valid_reset_password_path? ||
-      valid_registration_path? ||
-      valid_confirmation_path? ||
-      user_signed_in? ||
-      admin_signed_in?
-  end
 
   def after_sign_in_path_for(_resource)
     if admin_signed_in?
@@ -43,31 +28,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def redirect_signed_in
-  #   if admin_signed_in?
-  #     redirect_to admins_page_path
-  #   elsif user_signed_in?
-  #     redirect_to users_page_path
-  #   else
-  #     root_path
-  #   end
-  # end
-
-  def valid_reset_password_path?
-    request.path == edit_user_password_path && params[:reset_password_token].present?
-  end
-
-  def valid_registration_path?
-    request.path == new_user_registration_path && params[:registration_token].present?
-  end
-
-  def valid_confirmation_path?
-    request.path == user_confirmation_path && params[:confirmation_token].present?
-  end
-
   def authenticate_user!
     return if user_signed_in? || admin_signed_in?
-
-    redirect_to errors_forbidden_path
   end
 end
