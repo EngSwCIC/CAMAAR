468beed7021c848c0920c235a6745b1c1abf5611
jpantonov <jpantonow@gmail.com>
2024-06-07 13:33:53 -0300

    letter_opener_web

--- camaar/config/environments/development.rb
+++ camaar/config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: 'localhost', port: 3000
   }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
 
 
   config.action_mailer.raise_delivery_errors = false
