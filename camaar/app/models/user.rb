83f52c9d0e863469d979d37f42a79ffffb74af9e
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-08 13:40:35 -0300

    Created Matricula model and ran migration

--- camaar/app/models/user.rb
+++ camaar/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :matriculas
+  # has_many :turmas, through: :matriculas
 end
--- camaar/db/schema.rb
+++ camaar/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_203912) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_163747) do
+  create_table "matriculas", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_matriculas_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nome"
     t.string "email"
@@ -23,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_203912) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "matriculas", "users"
 end
