86a325a58f8521016c18b2d1bd4b03576f796a13
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-08 14:27:44 -0300

    Created Resposta model and ran migration

--- camaar/db/schema.rb
+++ camaar/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_171835) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_172544) do
   create_table "formularios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -31,6 +31,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_171835) do
   end
 
   create_table "questaos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "resposta", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
