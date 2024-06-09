79d1aed7941e3fe3f5fbd15ca3636a190c7df69e
jpantonov <jpantonow@gmail.com>
2024-06-09 17:48:48 -0300

    need to mount more args on set_user_data

--- camaar/app/controllers/application_controller.rb
+++ camaar/app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_user!, unless: :devise_controller?
+  # before_action :authenticate_user!, unless: :devise_controller?
   # before_action :authenticate_user!, unless: :allowed_path?
   # before_action :redirect_signed_in, only: [:index]
 
-  protected
-
-  def set_admin_data
-    @coordinator = Coordinator.find_by({ email: current_admin.email })
-    @department = Department.find_by_id(@coordinator.department_id) if @coordinator
+  def after_sign_in_path_for(_resource)
+    if admin_signed_in? # Assuming there is such a function
+      # admins_page_path
+      "/templates"
+    elsif user_signed_in?
+      '/users/page'
+    else
+      root_path
+    end
   end
+
+  protected
 
   def configure_permitted_parameters
     added_attrs = %i[username email password password_confirmation remember_me]
@@ -26,20 +31,26 @@ class ApplicationController < ActionController::Base
     @return_to = session[:return_to]
   end
 
-  private
+  def set_admin_data
+    @coordinator = Coordinator.find_by({ email: current_admin.email })
+    @department = Department.find_by_id(@coordinator.department_id) if @coordinator
+  end
 
-  def after_sign_in_path_for(_resource)
-    if admin_signed_in?
-      # admins_page_path
-      templates_path
-    elsif user_signed_in?
-      users_page_path
+  def set_user_data(resource)
+    student = Student.find_by(email: resource.email)
+    if student
+      @student = student
+      @department = Department.find_by_id(@coordinator.department_id) if @student
     else
-      root_path
+      @teacher = Teacher.find_by(email:resource.email)
+      @department = Department.find_by_id(@coordinator.department_id) if @teacher
     end
   end
+
 
   def authenticate_user!
     return if user_signed_in? || admin_signed_in?
   end
+
+
 end
