caa97c13cf81280481e4e7e4f152a0eb096e3a0e
jpantonov <jpantonow@gmail.com>
2024-06-09 16:19:06 -0300

    more rspec tests

--- camaar/spec/models/template_spec.rb
+++ camaar/spec/models/template_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Template, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'é válido com atributos válidos e FKS existentes' do
+    department = create(:department)
+
+      admin = create(:admin)
+      coordinator = create(:coordinator)
+      template = create(:template)
+      template_questions = create(:template_question)
+
+      expect(template).to be_valid
+      expect(template_questions).to be_valid
+
+  end
+
+  it 'é inválido sem FKS existentes' do
+    template = build(:template)
+    expect(template).to_not be_valid
+  end
 end
