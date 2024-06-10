d9ee619c3f788d3569bb87eac1571ccb35e95ede
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2024-06-10 17:47:39 -0300

    feat: first look

--- CAAMAR/config/routes.rb
+++ CAAMAR/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
