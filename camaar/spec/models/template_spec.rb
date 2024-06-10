447fbb5140b695bf8818f9f1a2a63bb6d9c842ba
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-10 18:44:50 -0300

    [rspec] #30 Testes para model Template

--- camaar/spec/models/template_spec.rb
+++ camaar/spec/models/template_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Template, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "para que seja válido" do
+    it "deve ser válido :)" do
+      template = create(:template)
+      expect(template).to be_valid
+    end
+
+    it "deve possuir um nome" do
+      templateB = build(:template, nome: nil)
+      expect(templateB).to_not be_valid
+    end
+  end
+
+  describe "para ter relações válidas" do
+    it "pode ter vários formulários" do
+      template = Template.reflect_on_association(:formularios)
+      expect(template.macro).to eq(:has_many)
+    end
+
+    it "pode ter várias questões" do
+      template = Template.reflect_on_association(:questaos)
+      expect(template.macro).to eq(:has_many)
+    end
+  end
 end
