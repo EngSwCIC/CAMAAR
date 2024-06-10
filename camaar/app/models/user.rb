c13c3619bfbab3b01768a46b0aa462e83da4284d
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-10 18:59:23 -0300

    [rspec] #23 Testes para model User sem devise

--- camaar/app/models/user.rb
+++ camaar/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  validates :nome, presence: true
+  validates :email, presence: true
+  validates :curso, presence: true
+  validates :matricula, presence: true
+  validates :formacao, presence: true
+  validates :ocupacao, presence: true
+
   has_many :matriculas
   has_many :turmas, through: :matriculas
 end
--- camaar/spec/models/user_spec.rb
+++ camaar/spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "para que seja válida" do
+    it "deve ser válida :)" do
+      user = create(:user)
+      expect(user).to be_valid
+    end
+
+    it "deve possuir um email" do
+      user = build(:user, email:nil)
+      expect(user).to_not  be_valid
+    end
+    it "o email deve ser válido" do
+      user = create(:user)
+      expect(user.email).to match(/\A[\w+]+@[a-z]+\.[a-z]+\z/i)
+    end
+
+    it "deve possuir uma matrícula" do
+      user = build(:user, matricula:nil)
+      expect(user).to_not  be_valid
+    end
+    it "a matrícula deve ser válida" do
+      user = create(:user)
+      expect(user.matricula).to match(/\A[0-9]{9}\z/)
+    end
+
+    it "deve fazer parte de um curso" do
+      user = build(:user, curso:nil)
+      expect(user).to_not  be_valid
+    end
+
+    it "deve possuir um nome" do
+      user = build(:user, nome:nil)
+      expect(user).to_not  be_valid
+    end
+
+    it "deve fazer parte de uma formação" do
+      user = build(:user, formacao:nil)
+      expect(user).to_not  be_valid
+    end
+
+    it "deve possuir uma ocupação" do
+      user = build(:user, ocupacao:nil)
+      expect(user).to_not  be_valid
+    end
+  end
+
+  describe "para ter relações válidas" do
+    it "pode ter várias matrículas" do
+      user = User.reflect_on_association(:matriculas)
+      expect(user.macro).to eq(:has_many)
+    end
+
+    it "pode ter várias turmas" do
+      user = User.reflect_on_association(:turmas)
+      expect(user.macro).to eq(:has_many)
+    end
+  end
 end
