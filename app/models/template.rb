b167e91fe6433e4c2fbfb0e39186f5a7fa68b354
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-11 14:56:19 -0300

    refactor: Remove old template references

--- app/models/template.rb
+++
@@ -1,7 +0,0 @@
-class Template < ApplicationRecord
-  has_many :questions, dependent: :destroy
-  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
-
-  validates :title, presence: true
-  validates :questions, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :templates
-
   resources :questions, only: [], param: :index do
     member do
       delete '(:id)' => "questions#destroy", as: ""
