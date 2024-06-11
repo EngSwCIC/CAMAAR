07af6bdd571fc7513573726012cee24d6b5dfa03
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-06-10 21:02:12 -0300

    autenticação funcionando

--- camaar/db/migrate/20240610135814_add_devise_to_users.rb
+++ camaar/db/migrate/20240610135814_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      #t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -38,7 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    #add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- camaar/db/schema.rb
+++ camaar/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_220346) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_135814) do
   create_table "formularios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -82,6 +82,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_220346) do
     t.string "ocupacao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "role"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "formularios", "templates"
