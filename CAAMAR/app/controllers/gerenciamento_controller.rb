9d9844de5d5a666ed4fc0da9d4ae7a4953449593
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2024-06-10 21:15:10 -0300

    feat: view de gerenciamento

---
+++ CAAMAR/app/controllers/gerenciamento_controller.rb
@@ -0,0 +1,5 @@
+class GerenciamentoController < ApplicationController
+    def show
+    end
+  end
+  
--- CAAMAR/config/routes.rb
+++ CAAMAR/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'main/index'
+  get 'gerenciamento', to: 'gerenciamento#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
