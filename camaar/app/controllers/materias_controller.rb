aea24151bd29336065c19d2783d1510bb6250181
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-06-12 18:33:31 +0000

    MateriasController show & index #39

--- camaar/app/controllers/materias_controller.rb
+++ camaar/app/controllers/materias_controller.rb
@@ -1,2 +1,11 @@
 class MateriasController < ApplicationController
+    def index
+        @materias = Materia.all
+        render json: @materias
+    end
+    def show
+        @materia = Materia.find(params[:id])
+        render json: @materia
+    end
 end
+
--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
 
+  resources :materias
+
 end
