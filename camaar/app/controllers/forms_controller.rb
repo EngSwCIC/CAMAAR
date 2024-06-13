429b71a4cbc46afb9b32df48ff61c60242ddf065
jpantonov <jpantonow@gmail.com>
2024-06-13 09:03:21 -0300

    changes on layout of users forms

--- camaar/app/controllers/forms_controller.rb
+++ camaar/app/controllers/forms_controller.rb
@@ -1,29 +1,27 @@
 class FormsController < ApplicationController
   before_action :set_user_data
 
-  layout "user"
+  layout 'user'
 
   def index
     @errors = []
 
     occupation = current_user.occupation
-
     case occupation
-    when "discente"
+    when 'discente'
       @enrollments = Enrollment.where(student_id: @student.id)
-
       if @enrollments.empty?
-        @errors << "Parece que você não está matriculado em nenhum disciplina."
+        @errors << 'Parece que você não está matriculado em nenhum disciplina.'
       else
         @subject_classes = SubjectClass.where({ id: @enrollments.pluck(:subject_class_id) })
       end
-    when "docente"
+    when 'docente'
       @subject_classes = SubjectClass.where(teacher_id: @teacher.id)
     end
 
     if @subject_classes.blank?
       @forms = []
-      @errors << "Nenhum formulário encontrado."
+      @errors << 'Nenhum formulário encontrado.'
     else
       @forms = Form.where(subject_class_id: @subject_classes.pluck(:id), role: occupation)
     end
@@ -36,9 +34,9 @@ class FormsController < ApplicationController
 
   def export(form)
     create_graph(form)
-    temp_file = Tempfile.new(["pie_graph", ".png"])
+    temp_file = Tempfile.new(['pie_graph', '.png'])
     graph.write(temp_file.path)
-    send_file temp_file.path, type: "image/png", disposition: "attachment", filename: "pie_graph.png"
+    send_file temp_file.path, type: 'image/png', disposition: 'attachment', filename: 'pie_graph.png'
     temp_file.close
     temp_file.unlink
   end
@@ -46,11 +44,11 @@ class FormsController < ApplicationController
   def create_graph(_form)
     # export a graph
     graph = Gruff::Pie.new
-    graph.title = "Respostas do Formulário"
+    graph.title = 'Respostas do Formulário'
     num_students = Student.count
     num_answers = StudentAnswer.where(form_question_id: form_question.id).count
-    graph.data "Total de ausências", num_students - num_answers
-    graph.data "Total de respostas", num_answers
-    graph.write("pie_graph.png")
+    graph.data 'Total de ausências', num_students - num_answers
+    graph.data 'Total de respostas', num_answers
+    graph.write('pie_graph.png')
   end
 end
