3667b7f5ca02c38f8a94f75f72b67fec1c5d43ff
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-14 16:24:14 -0300

    feat: Add seeding with faker

--- Gemfile
+++ Gemfile
@@ -1,33 +1,34 @@
-ruby "3.1.2"
-source "https://rubygems.org"
+ruby '3.1.2'
+source 'https://rubygems.org'
 
 #gems
 gem 'bcrypt'
 gem 'mysql2'
 gem 'bundler-audit'
-gem "rails", "~> 7.1.3.4"
-gem "sprockets-rails"
-gem "puma", ">= 5.0"
-gem "importmap-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
-gem "jbuilder"
-gem "redis", ">= 4.0.1"
-gem "tzinfo-data", platforms: %i[ windows jruby ]
-gem "bootsnap", require: false
-gem "haml"
-gem "cucumber"
+gem 'rails', '~> 7.1.3.4'
+gem 'sprockets-rails'
+gem 'puma', '>= 5.0'
+gem 'importmap-rails'
+gem 'turbo-rails'
+gem 'stimulus-rails'
+gem 'jbuilder'
+gem 'redis', '>= 4.0.1'
+gem 'tzinfo-data', platforms: %i[ windows jruby ]
+gem 'bootsnap', require: false
+gem 'haml'
+gem 'cucumber'
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri windows ]
-  gem 'rubocop'
+  gem 'debug', platforms: %i[ mri windows ]
+  gem 'faker'
 end
 
 group :development do
-  gem "web-console"
+  gem 'web-console'
+  gem 'rubocop'
 end
 
 group :test do
-  gem "capybara"
-  gem "selenium-webdriver"
+  gem 'capybara'
+  gem 'selenium-webdriver'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+require 'faker'
+
+# Constants/settings
+CURRENT_YEAR = Time.now.year
+YEARS = 5
+TEMPLATES_PER_SEMESTER = 3
+
+# Generate semesters for the next $YEAR years
+(0..YEARS).each do |year_offset|
+  year = CURRENT_YEAR + year_offset
+  (1..2).each do |semester_number|
+    half = semester_number == 2
+    Semester.create half:, year:
+  end
+end
+
+# Generate $TEMPLATES_PER_SEMESTER templates
+Semester.find_each do |semester|
+  TEMPLATES_PER_SEMESTER.times do
+    Template.create semester:
+  end
+end
+
+# Generate 1-15 questions per template
+Template.find_each do |template|
+  rand(1..15).times do
+    Question.create template:,
+                    label: Faker::Lorem.words(number: rand(3..7)).join(' '),
+                    description: Faker::Lorem.paragraphs(number: rand(1..3)).join(' ')
+  end
+end
