d754cdcc989436979999a7670ca0328d656cccfd
Mateus Santos <santos.mateus@protonmail.com>
2024-06-14 14:55:47 -0300

    added avaliacoes page

--- app/controllers/avaliacoes_controller.rb
+++ app/controllers/avaliacoes_controller.rb
@@ -1,2 +1,4 @@
 class AvaliacoesController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get '/avaliacoes', to: 'avaliacoes#index'
+
   root to: "home#index"
 end
