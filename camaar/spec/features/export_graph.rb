70a319b818e467df47076683d8cb7618dbbcb0f6
jpantonov <jpantonow@gmail.com>
2024-06-16 16:39:24 -0300

    new rchart

--- camaar/spec/features/export_graph.rb
+++ camaar/spec/features/export_graph.rb
@@ -1,47 +1,62 @@
-# require 'rails_helper'
+require 'rails_helper'
 # require 'gruff'
+require 'rchart'
+RSpec.feature 'Export Graph', type: :feature do
+  describe 'exporting answers as graph' do
+    it 'should export a graph containing answers % by students' do
+      department = create(:department, :departament1)
 
-# RSpec.feature 'Export Graph', type: :feature do
-#   describe 'exporting answers as graph' do
-#     it 'should export a graph containing answers % by students' do
-#       graph = Gruff::Pie.new
-#       graph.title = 'Respostas do Formulário'
+      admin = create(:admin, :admin1)
 
-#       department = create(:department,:department1)
+      user1 = create(:user, :user1)
+      user2 = create(:user, :user2)
+      user3 = create(:user, :user3)
+      user4 = create(:user, :user4)
+      user5 = create(:user, :user5)
 
-#       admin = create(:admin, :admin1)
+      student1 = create(:student, :student1)
+      student2 = create(:student, :student2)
+      student3 = create(:student, :student3)
+      student4 = create(:student, :student4)
 
-#       user1 = create(:user, :user1)
-#       user2 = create(:user, :user2)
-#       user3 = create(:user, :user3)
-#       user4 = create(:user, :user4)
-#       user5 = create(:user, :user5)
+      coordinator = create(:coordinator, :coordinator1)
+      template = create(:template, :template1)
+      template_questions = create(:template_question, :template_question1)
 
-#       student1 = create(:student, :student1)
-#       student2 = create(:student, :student2)
-#       student3 = create(:student, :student3)
-#       student4 = create(:student, :student4)
+      teacher = create(:teacher, :teacher1)
+      subject_class1 = create(:subject_class, :subject_class1)
 
-#       coordinator = create(:coordinator,:coordinator1)
-#       template = create(:template, :template1)
-#       template_questions = create(:template_question, :template_question1)
+      form = create(:form, :form1)
+      form_question = create(:form_question, :form_question1)
 
-#       teacher = create(:teacher,:teacher1)
-#       subject_class1 = create(:subject_class,:subject_class1)
+      answ1 = create(:student_answer, :student_answers1)
+      answ2 = create(:student_answer, :student_answers2)
+      answ3 = create(:student_answer, :student_answers3)
 
-#       form = create(:form,:form1)
-#       form_question = create(:form_question,:form_question1)
+      num_students = Student.count
+      num_answers = StudentAnswer.where(form_question_id: form_question.id).count
+      num_absents = num_students - num_answers
 
-#       answ1 = create(:student_answer, :student_answers1)
-#       answ2 = create(:student_answwwer, :student_answers2)
-#       answ3 = create(:student_answer, :student_answers3)
+      p = Rdata.new
+      p.add_point([num_answers, num_absents], 'Serie1')
+      p.add_point(%w[Respostas Ausências], 'Serie2')
+      p.add_all_series
+      p.set_abscise_label_serie('Serie2')
 
-#       num_students = Student.count
-#       num_answers = StudentAnswer.where(form_question_id: form_question.id).count
+      ch = Rchart.new(300, 200)
+      ch.set_font_properties('tahoma.ttf', 8)
+      ch.draw_filled_rounded_rectangle(7, 7, 293, 193, 5, 240, 240, 240)
+      ch.draw_rounded_rectangle(5, 5, 295, 195, 5, 230, 230, 230)
 
-#       graph.data 'Total de ausências', num_students - num_answers
-#       graph.data 'Total de respostas', num_answers
-#       graph.write('pie_graph.png')
-#     end
-#   end
-# end
+      # Draw the pie chart
+      ch.antialias_quality = 0
+      ch.set_shadow_properties(2, 2, 200, 200, 200)
+      ch.draw_flat_pie_graph_with_shadow(p.get_data, p.get_data_description, 120, 100, 60, Rchart::PIE_PERCENTAGE, 8)
+      ch.clear_shadow
+
+      ch.draw_pie_legend(210, 35, p.get_data, p.get_data_description, 250, 250, 250)
+
+      ch.render_png(form.name)
+    end
+  end
+end
