f7bba223d1f8aa0b92bad3a6ba664ac8caa34753
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-06-17 19:44:43 -0300

    adicionando seeds com factorybot(não testado)

--- camaar/db/seeds.rb
+++ camaar/db/seeds.rb
@@ -22,3 +22,47 @@ matricula: "190085006", formacao: "graduando", ocupacao: "dicente",
 password: "123456789",role: :user,
 password_confirmation: "123456789", reset_password_token: nil,
 reset_password_sent_at: nil, remember_created_at: nil)
+
+
+create_list(:user, 5)
+
+# Create materias
+create_list(:materia, 3)
+
+# Create turmas with associated materias
+create_list(:turma, 4) do |turma|
+  turma.materia = Materia.all.sample
+end
+
+# Create templates
+create_list(:template, 2)
+
+# Create formularios with associations
+create_list(:formulario, 5) do |formulario|
+  # Assign random turma and template
+  formulario.turma = Turma.all.sample
+  formulario.template = Template.all.sample
+end
+
+# Create questoes with associations
+Questao.all.each do |questao|
+  # Skip creation if no formulario exists (prevents errors)
+  next unless formulario = Formulario.all.sample
+  questao.formulario = formulario
+
+  # Optionally assign a random template (modify if needed)
+  questao.template = Template.all.sample
+end
+
+# Create respostas with associations
+Resposta.all.each do |resposta|
+  # Skip creation if no questao exists (prevents errors)
+  next unless questao = Questao.all.sample
+  resposta.questao = questao
+end
+
+create_list(:matricula, 3) do |matricula|
+  # Assign random user and turma
+  matricula.user = User.all.sample
+  matricula.turma = Turma.all.sample
+end
