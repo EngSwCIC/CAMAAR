1ffbf33cb74bb47bff91c33f899c2e36fb8a9f90
llaurxx <arual352003@gmail.com>
2024-06-16 23:09:26 -0300

    Telas inicial, Primeiro acesso, recuperação de senha #45

--- camaar/spec/factories/users.rb
+++ camaar/spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     nome { "Ana Clara Jordao Perna" }
     email { "acjpjvjp@gmail.com" }
+    password	{ "123456" }
     curso { "CIÊNCIA DA COMPUTAÇÃO/CIC" }
     matricula { "190084006" }
     formacao { "graduando" }
