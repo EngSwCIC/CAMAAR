e82cedcbaaa8ae2bcc23ed5959178d138248b99d
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-18 18:16:49 -0300

    feat: Send form input from view to controller

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
-# Controls routes for templates.
+# Controls routes for templates. Only administrators may see or edit templates.
 class TemplatesController < ApplicationController
+  include ManagerHelper
   include AuthenticationConcern
 
-  def index
-    return unless user_authenticated
+  # TODO: add authentication with `return unless user_authenticated && admin_user?`
 
+  def index
     @templates = Template.all
   end
 
   def show
     @template = Template.find(params[:id])
+  end
+
+  def create
+    Rails.logger.debug("Received: #{params[:questions].inspect}")
+  end
+
+  def update
+    Rails.logger.debug("Received: #{params[:questions].inspect}")
   end
 end
