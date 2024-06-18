f8a42a2b95d0f28bd04ff9be0f9627a6b0746ef0
jpantonow <jpantonow@gmail.com>
2024-06-18 11:20:54 -0300

    test for results page done

---
+++ camaar/spec/factories/enrollments.rb
@@ -0,0 +1,30 @@
+FactoryBot.define do
+  factory :enrollment do
+    created_at { Time.now.utc }
+    updated_at { Time.now.utc }
+
+    trait :enrollment1 do
+      id { 1 }
+      student_id {1}
+      subject_class_id {1}
+    end
+
+    trait :enrollment2 do
+      id { 2 }
+      student_id {2}
+      subject_class_id {1}
+    end
+
+    trait :enrollment3 do
+      id { 3 }
+      student_id {3}
+      subject_class_id {1}
+    end
+
+    trait :enrollment4 do
+      id { 4 }
+      student_id {8}
+      subject_class_id {1}
+    end
+  end
+end
