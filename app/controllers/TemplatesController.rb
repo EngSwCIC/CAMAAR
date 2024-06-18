c7ac223d74a8736ee920bb738898784f75bc6baf
TagFernandes <tagfernandes@hotmail.com>
2024-06-18 13:49:20 -0400

    Vizualização de respostas

---
+++ app/controllers/TemplatesController.rb
@@ -0,0 +1,25 @@
+class TemplatesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def save_template
+    template_data = params.require(:template).permit(:templateName, questions: [:type, { options: [] }])
+    file_name = "#{template_data[:templateName]}.json"
+    file_path = Rails.root.join('public', 'templates', file_name)
+
+    File.open(file_path, 'w') do |file|
+      file.write(JSON.pretty_generate(template_data.to_h))
+    end
+
+    render plain: 'Template salvo com sucesso!'
+  rescue => e
+    logger.error "Erro ao salvar o template: #{e.message}"
+    render plain: 'Erro ao salvar o template', status: :internal_server_error
+  end
+
+  def check_name
+    template_name = params[:name]
+    template_exists = Template.exists?(name: template_name)
+    render json: { exists: template_exists }
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  get 'get_response_files', to: 'formularios#get_response_files'
+  get 'list_json_files', to: 'formularios#list_json_files'
+
+  get 'count_responses', to: 'formularios#count_responses'
+
+
+  post 'submit_form', to: 'reports_alunos#submit_form'
+
+  resources :formularios do
+    collection do
+      post :save_formulario
+    end
+  end
+
+  get 'reports_alunos/formularios', to: 'reports_alunos#formularios'
+  get 'fetch_form', to: 'reports_alunos#fetch_form'
+
+  get 'view_file_form', to: 'formularios#view_file_form'
+  delete 'delete_file_form', to: 'formularios#delete_file_form'
+  # Adicione a rota para criação de novos formulários se necessário
+  get 'new_formulario', to: 'formularios#new'
+  get 'formularios', to: 'formularios#index'
+  
+  resources :formularios
+  post 'save_formulario', to: 'formularios#save_formulario'
+
+  post 'save_template', to: 'templates#save_template'
+
+  get 'formulario_templates/index'
+  get 'formulario_templates/edit_template', to: 'formulario_templates#edit_template', as: 'edit_template'
+  root 'formulario_templates#index'
+
+  delete 'delete_file', to: 'formulario_templates#delete_file'
+  get 'view_file', to: 'formulario_templates#view_file'
+
+  get 'templates/check_name', to: 'templates#check_name'
+
   resources :reports_professors
   resources :reports_alunos
   resources :formulario_templates
