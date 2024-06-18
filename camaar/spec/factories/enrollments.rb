a6d4d82f7a9143e58d439559b4c24106094d2bcc
jpantonow <jpantonow@gmail.com>
2024-06-18 17:02:43 -0300

    rspecing

--- camaar/spec/factories/enrollments.rb
+++ camaar/spec/factories/enrollments.rb
@@ -5,26 +5,31 @@ FactoryBot.define do
 
     trait :enrollment1 do
       id { 1 }
-      student_id {1}
-      subject_class_id {1}
+      student_id { 1 }
+      subject_class_id { 1 }
     end
 
     trait :enrollment2 do
       id { 2 }
-      student_id {2}
-      subject_class_id {1}
+      student_id { 2 }
+      subject_class_id { 1 }
     end
 
     trait :enrollment3 do
       id { 3 }
-      student_id {3}
-      subject_class_id {1}
+      student_id { 3 }
+      subject_class_id { 1 }
     end
 
     trait :enrollment4 do
       id { 4 }
-      student_id {8}
-      subject_class_id {1}
+      student_id { 8 }
+      subject_class_id { 1 }
+    end
+    trait :enrollment5 do
+      id { 4 }
+      student_id { 8 }
+      subject_class_id { 2 }
     end
   end
 end
---
+++ camaar/spec/features/answer_form.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+feature 'Answer forms' do
+  background do
+    # will clear the message queue
+    clear_emails
+  end
+
+  before do
+    departament = create(:department, :departament1)
+    admin = create(:admin, :admin1)
+    coordinator = create(:coordinator, :coordinator1)
+    user = create(:user, :user4)
+    student4 = create(:student, :student4)
+    template = create(:template, :template1)
+    template_questions = create(:template_question, :template_question1)
+    user = create(:user, :user6)
+    teacher = create(:teacher, :teacher2)
+    subject_class1 = create(:subject_class, :subject_class2)
+    enrollment = create(:enrollment, :enrollment5)
+    visit '/admins/login'
+    fill_in 'email', with: admin.email
+    fill_in 'password', with: admin.password
+    click_button 'Confirmar'
+    click_link 'Envio'
+    select 'Template1', from: 'aluno_template_id'
+    page.check('BANCOS DE DADOS')
+    click_button 'Enviar'
+    click_link 'Sair'
+  end
+  scenario 'admin can sends forms to students' do
+    visit '/users/login'
+    user = build(:user, :user4)
+    template = build(:template, :template1)
+    template_question = build(:template_question, :template_question1)
+    expect(page).to have_content 'Bem vindo ao'
+    expect(page).to have_content 'CAMAAR'
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    click_button 'Confirmar'
+
+    expect(page).to have_content('Formulários Pendentes')
+
+    click_link template.name
+    expect(page).to have_content template.name
+    expect(page).to have_content template_question.title
+    fill_in template_question.id, with: 'fine'
+    click_button 'Enviar'
+    expect(page).to have_content 'Formulários Pendentes'
+    expect(page).to_not have_content template.name
+  end
+end
