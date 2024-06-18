3cc9910831f9fb960e98052541ecabd94a986657
jpantonow <jpantonow@gmail.com>
2024-06-18 04:28:11 -0300

    cleaning rspec

---
+++ camaar/spec/features/results.rb
@@ -0,0 +1,22 @@
+# require 'rails_helper'
+
+# RSpec.feature 'Delete a template', type: :feature do
+#   before do
+#     department = create(:department, :departament1)
+#     admin = create(:admin, :admin1)
+#     coordinator = create(:coordinator, :coordinator1)
+#     student =
+#     visit '/admins/login'
+
+#     expect(page).to have_content 'Bem vindo ao'
+#     expect(page).to have_content 'CAMAAR'
+#     fill_in 'email', with: admin.email
+#     fill_in 'password', with: admin.password
+#     click_button 'Confirmar'
+#   end
+#   describe 'admin can export a csv with answers ' do
+#     it 'should export a csv with answers of questions' do
+
+#     end
+#   end
+# end
