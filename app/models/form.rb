6c23ec08261f13ca188b5340c31c5d111621b5fe
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-19 13:33:57 -0300

    feat: Add form creation from template

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,38 @@
 class Form < ApplicationRecord
   belongs_to :template, optional: true
   has_many :questions, as: :formlike
-  belongs_to :semester
+  belongs_to :semester, optional: true
+
+  validate :semester_required
+  before_create :import_template_data
+
+  private
+
+  def semester_required
+    return unless template.nil? && semester.nil?
+
+    errors.add(:semester, "can't be nil when template is also nil")
+  end
+
+  def import_template_data
+    return if template.nil?
+
+    self.semester = template.semester
+    template.questions.each do |question|
+      question_params = {
+        formlike: self,
+        label: question.label,
+        description: question.description,
+        format: question.format
+      }
+      questions << case question.type
+                   when 'MultipleChoiceQuestion'
+                     MultipleChoiceQuestion.new(question_params)
+                   when 'TextInputQuestion'
+                     TextInputQuestion.new(question_params)
+                   else # Usually a 'Question' class
+                     Question.new(question_params)
+                   end
+    end
+  end
 end
