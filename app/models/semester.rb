c66b474075337af32e1a87bbc3e80b39c8a41bc7
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-18 22:59:42 -0300

    fix: Add back to_string and validation to semester model

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,43 +1,48 @@
+# frozen_string_literal: true
+
 class Semester < ApplicationRecord
+  validates :half, uniqueness: { scope: :year }
+  has_many :templates
+
+  def to_s
+    "#{half ? '2' : '1'}-#{year}"
+  end
+
   def self.next_semester_id
-    latest_semester = self.order(id: :desc).first
+    latest_semester = order(id: :desc).first
 
     if latest_semester.nil?
       half = 0
       year = 2020
+    elsif latest_semester.half
+      half = 0
+      year = latest_semester.year + 1
     else
-      if latest_semester.half
-        half = 0
-        year = latest_semester.year + 1
-      else
-        half = 1
-        year = latest_semester.year
-      end
+      half = 1
+      year = latest_semester.year
     end
-    new_semester = self.create(half: half, year: year)
-    return new_semester.id
+    new_semester = create(half:, year:)
+    new_semester.id
   end
 
   def self.current_semester
-    latest_semester = self.order(id: :desc).first
-    if latest_semester 
-      if latest_semester.half
-        half = 2
-      else
-        half = 1
-      end
-        "#{latest_semester.year}.#{half}."
+    latest_semester = order(id: :desc).first
+    if latest_semester
+      half = if latest_semester.half
+               2
+             else
+               1
+             end
+      "#{latest_semester.year}.#{half}."
     else
-      "Sem semestre cadastrado."
+      'Sem semestre cadastrado.'
     end
   end
 
   def self.current_semester_id
-    latest_semester = self.order(id: :desc).first
-    if latest_semester
-      return latest_semester.id
-    else
-      return latest_semester
-    end
+    latest_semester = order(id: :desc).first
+    return latest_semester.id if latest_semester
+
+    latest_semester
   end
 end
