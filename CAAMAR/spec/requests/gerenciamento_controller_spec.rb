a446fce5e830d18ed1acbea25b98b4d180c6a458
Elaina <17bestradiol@proton.me>
2024-06-20 20:00:46 -0300

    Adding some tests

--- CAAMAR/spec/requests/gerenciamento_controller_spec.rb
+++ CAAMAR/spec/requests/gerenciamento_controller_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe "GerenciamentoController", type: :request do
       expect(response).to have_http_status(:ok).or have_http_status(302)
     end
   end
+
+  describe "GET /gerenciamento/results_popup" do
+    it "returns http success" do
+      get "/gerenciamento/results_popup"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /gerenciamento/generate_csv" do
+    it "returns http success" do
+      get "/gerenciamento/generate_csv"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to start_with('application/zip')
+    end
+  end
 end
