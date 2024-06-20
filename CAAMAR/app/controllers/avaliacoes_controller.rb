303716e2046a55e42c62bba93680880b14c33865
Elaina <17bestradiol@proton.me>
2024-06-20 14:21:03 -0300

    Transforming getting form info into a helper method

--- CAAMAR/app/controllers/avaliacoes_controller.rb
+++ CAAMAR/app/controllers/avaliacoes_controller.rb
@@ -8,24 +8,6 @@ class AvaliacoesController < ApplicationController
       !solver.nil? && !solver["is_solved"]
     end
 
-    classes = JSON.parse(File.read('db/json/classes.json'))
-    class_members = JSON.parse(File.read('db/json/class_members.json'))
-    # Add class_data to the filtered forms
-    @forms = unanswered_forms.map do |form|
-      class_data = classes.find do |info|
-        info["code"] == form["class"]["subject_code"] &&
-        info["class"]["semester"] == form["class"]["semester"] &&
-        info["class"]["classCode"] == form["class"]["code"]
-      end
-      
-      teacher_name = (class_members.find do |info|
-        info["code"] == form["class"]["subject_code"] &&
-        info["semester"] == form["class"]["semester"] &&
-        info["classCode"] == form["class"]["code"]
-      end)["docente"]["nome"]
-      class_data["class"]["teacher_name"] = teacher_name
-
-      form.merge("class_data" => class_data)
-    end
+    @forms = helpers.get_forms_info(unanswered_forms)
   end
 end
--- CAAMAR/app/helpers/application_helper.rb
+++ CAAMAR/app/helpers/application_helper.rb
@@ -8,4 +8,26 @@ module ApplicationHelper
     def get_current_user_id()
         1
     end
+
+    def get_forms_info(forms)
+        classes = JSON.parse(File.read('db/json/classes.json'))
+        class_members = JSON.parse(File.read('db/json/class_members.json'))
+        # Add class_data to the filtered forms
+        forms.map do |form|
+            class_data = classes.find do |info|
+                info["code"] == form["class"]["subject_code"] &&
+                info["class"]["semester"] == form["class"]["semester"] &&
+                info["class"]["classCode"] == form["class"]["code"]
+            end
+            
+            teacher_name = (class_members.find do |info|
+                info["code"] == form["class"]["subject_code"] &&
+                info["semester"] == form["class"]["semester"] &&
+                info["classCode"] == form["class"]["code"]
+            end)["docente"]["nome"]
+            class_data["class"]["teacher_name"] = teacher_name
+
+            form.merge("class_data" => class_data)
+        end
+    end
 end
