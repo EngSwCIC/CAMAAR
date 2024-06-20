525bf0e2becf43dbd52860458a52740b4cc95142
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2024-06-20 11:10:30 -0300

    Revert "Merge branch 'sprint-2' of https://github.com/EduardoMarciano/QuickClassEvalFork into sprint-2"
    This reverts commit 0f68bd804afbcbdffd4b0564c7088081cfa90381, reversing
    changes made to 9c747406b532a69f000bcadfa21f1b02e65f2929.

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -22,24 +22,4 @@ class TemplatesController < ApplicationController
   def update
     Rails.logger.debug("Received: #{params[:questions].inspect}")
   end
-
-  # GET: Page for sending a form to students of a discipline
-  def edit_send
-    redirect_to root_path unless user_authenticated && admin_user?
-
-    @disciplines = Discipline.all
-    @templates = Template.all
-    render 'templates/send/index'
-  end
-
-  # POST: Gives students of disciplines access to a form copied from the given template
-  def send_out_forms
-    params.permit(:id, discipline_ids: %i[id])
-    return unless user_authenticated && admin_user? && params.key?(:discipline_ids)
-
-    form = Form.create template: Template.find(params[:id])
-    params[:discipline_ids].each do |discipline_id|
-      FormDiscipline.create form:, discipline: Discipline.find(discipline_id)
-    end
-  end
 end
