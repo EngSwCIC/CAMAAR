574b866037af68ad6d4532d3c21f4790cee43759
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-06-20 04:34:59 +0000

    #46 Reposnder formulario

--- camaar/app/controllers/formularios_controller.rb
+++ camaar/app/controllers/formularios_controller.rb
@@ -1,5 +1,61 @@
 class FormulariosController < ApplicationController
     def index
-        @formularios = Formulario.all
+        formularios = Formulario.all
+        render json: formularios, status: :ok
+    end
+
+    def show
+        formulario = Formulario.find(params[:id])
+        render json: formulario, status: :ok
+    rescue StandardError => e
+        render json: e, status: :not_found
+    end
+
+    def create
+        formulario = Formulario.new(formulario_params)
+        formulario.save!
+        render json: formulario, status: :created
+    rescue StandardError => e
+        render json: e, status: :bad_request
+    end
+
+    def update
+        formulario = Formulario.find(params[:id])
+        formulario.update!(formulario_params)
+        render json: formulario, status: :ok
+    rescue StandardError => e
+        render json: e, status: :bad_request
+    end
+
+    def delete
+        formulario = Formulario.find(params[:id])
+        formulario.destroy!
+        render json: { message: "Formulario deleted." }, status: :ok
+    rescue StandardError => e
+        render json: e, status: :not_found
+    end
+
+    def responder
+        @formulario = Formulario.find(params[:id])
+    end
+
+    def resposta
+        formulario = Formulario.find(params[:id])
+      
+        params[:formulario][:questaos_attributes].each do |key, resposta|
+            questao = Questao.find(resposta[:id])
+            resposta = Resposta.new({ "formulario_id": formulario.id, "questao_id": questao.id, "texto": resposta[:resposta][:value]})
+            resposta.save!
+        end
+        
+      
+        # Handle successful form submission (e.g., redirect)
+        redirect_to '/'
+    end
+
+    private
+
+    def formulario_params
+        params.require(:formulario).permit(:nome, :turma_id, :template_id)
     end
 end
--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -13,6 +13,58 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
 
+  scope "/turmas" do
+    get "/", to: "turmas#index"
+    get "/:id", to: "turmas#show"
+    # post "/", to: "turmas#create"
+    # patch "/:id", to: "turmas#update"
+    # delete "/:id", to: "turmas#destroy"
+  end
+
+  scope "/matriculas" do
+    post "/", to: "matriculas#create"
+    delete "/:id", to: "matriculas#delete"
+    get "/", to: "matriculas#index"
+    # get "/:id", to: "matriculas#show"
+    # patch "/:id", to: "matriculas#update"
+  end
+
+  resources :formulario
+  scope "/formularios" do
+    get "/", to: "formularios#index"
+    get "/:id", to: "formularios#show"
+    post "/", to: "formularios#create"
+    patch "/:id", to: "formularios#update"
+    delete "/:id", to: "formularios#delete"
+    get "/responder/:id", to: "formularios#responder"
+    post "/responder/:id", to: "formularios#resposta"
+  end
+
+  scope "/questoes" do
+    get "/", to: "questoes#index"
+    get "/:id", to: "questoes#show"
+    post "/", to: "questoes#create"
+    patch "/:id", to: "questoes#update"
+    delete "/:id", to: "questoes#delete"
+  end
+
+  scope "/respostas" do
+    get "/", to: "respostas#index"
+    get "/:id", to: "respostas#show"
+    post "/", to: "respostas#create"
+    patch "/:id", to: "respostas#update"
+    delete "/:id", to: "respostas#delete"
+  end
+
+  scope "/users" do
+    get "/", to: "users#index"
+    get "/:id", to: "users#show"
+    post "/", to: "users#create"
+    patch "/:id", to: "users#update"
+    delete "/:id", to: "users#delete"
+  end
+
+  resources :templates
   resources :materias
 
 end
