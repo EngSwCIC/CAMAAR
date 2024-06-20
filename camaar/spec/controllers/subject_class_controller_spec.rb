8d34452400737d00d3b35059e02d10e2c4e28687
jpantonow <jpantonow@gmail.com>
2024-06-20 20:52:14 -0300

    increasing coverage on rspec

--- camaar/spec/controllers/subject_class_controller_spec.rb
+++ camaar/spec/controllers/subject_class_controller_spec.rb
@@ -10,19 +10,29 @@ RSpec.describe SubjectClassesController, type: :controller do
     end
   end
 
-  # describe 'admin with no dpto or classes cant see' do
-  #   let(:admin) { create(:admin, :admin1) }
-  #   before do
-  #     allow(controller).to receive(:authenticate_admin!).and_return(true)
-  #     allow(controller).to receive(:current_admin).and_return(admin)
-  #     @request.env['devise.mapping'] = Devise.mappings[:admin]
-  #   end
+  describe 'admin that is not a coordinator cannot access' do
+    let(:admin) { create(:admin, :admin1) }
+    before do
+      allow(controller).to receive(:authenticate_admin!).and_return(true)
+      allow(controller).to receive(:current_admin).and_return(admin)
+      @request.env['devise.mapping'] = Devise.mappings[:admin]
+    end
 
-  #   it 'should not see classes' do # Added `do` here
-  #     sign_in admin
-  #     get :index
-  #     expect(response).to have_http_status(:forbidden)
-  #     expect(page).to have_content 'zeca'
-  #   end
-  # end
+    it 'should not see classes' do # Added `do` here
+      sign_in admin
+      get :index
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'should get index' do
+      departament = create(:department, :departament1)
+      coordinator = create(:coordinator, :coordinator1)
+      user = create(:user, :user5)
+      teacher = create(:teacher, :teacher1)
+      classes = create(:subject_class, :subject_class1)
+      sign_in admin
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
