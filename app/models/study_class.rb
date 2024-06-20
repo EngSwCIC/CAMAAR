3d92b0563c9c16339c063c6e57d06df1cd13dc0a
Mateus Santos <santos.mateus@protonmail.com>
2024-06-20 15:56:26 -0300

    Fixed BDD for view pending forms

--- app/models/study_class.rb
+++ app/models/study_class.rb
@@ -1,8 +1,9 @@
 class StudyClass < ActiveRecord::Base
   has_and_belongs_to_many :users
+  belongs_to :docente, :class_name => :User, :foreign_key => "docente_id"
 
-  def docente
-    # retorna uma instancia de user em vez de ser so o id
-    User.find(self[:docente])
-  end
+  # def docente
+  #   # retorna uma instancia de user em vez de ser so o id
+  #   User.find(@docente_id)
+  # end
 end
