39ab5e8491f7c2b75f30f205c370749279df65f8
Mateus Santos <santos.mateus@protonmail.com>
2024-06-21 11:21:54 -0300

    fixed view pending forms steps

--- features/step_definitions/view_pending_forms_steps.rb
+++ features/step_definitions/view_pending_forms_steps.rb
@@ -1,38 +1,29 @@
 Given('I am logged in as a student from class {string}') do |class_name|
-  @user = User.new(
-    :email => "teste@email.com",
-    :matricula => "123456",
-    :password => "senha123",
-    :password_confirmation => "senha123"
-  )
+  @user = FactoryBot.build(:user)
+  @user.email = "teste@email.com"
 
-  @docente = User.create(
-    :email => "docente@email.com",
-    :matricula => "abc123",
-    :password => "senha123",
-    :password_confirmation => "senha123"
-  )
+  @docente = FactoryBot.build(:user)
+  @docente.email = "docente@email.com"
+  @docente.save!
 
-  @study_class = StudyClass.create(
-    :code => "cic01",
-    :name => class_name,
-    :classCode => "es123",
-    :semester => "2024.1",
-    :time => "Time",
-  )
+  @study_class = FactoryBot.build(:study_class)
+  @study_class.name = class_name
   @study_class.docente = @docente
+  @study_class.save!
 
   @user.study_classes << @study_class
-
   @user.save!
 
   login_as(@user, scope: :user)
 end
 
 Given('a form has been sent to this user') do
+  @template = FactoryBot.build(:template)
+
   @form_request = FormRequest.new
   @form_request.user = @user
   @form_request.study_class = @study_class
+  @form_request.template = @template
   @form_request.save!
 end
 
