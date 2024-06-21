ab1fffca7d9a802f9f60172fd66ec4d14b3c46fa
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-21 17:29:03 -0300

    test: Add test for getting to the send forms page

---
+++ features/step_definitions/form_steps.rb
@@ -0,0 +1,4 @@
+When('I send forms') do
+  step 'I am on the Send page'
+  pending 'TODO'
+end
--- features/step_definitions/page_steps.rb
+++ features/step_definitions/page_steps.rb
@@ -1,16 +1,35 @@
-Given(/^I am on the(?: main)? (.*) page$/) do |page_name|
+Given(/^I am on the(?: main)? (\w+) page$/) do |page_name|
   case page_name.downcase
   when 'management'
+    step 'I am an administrator'
     visit manager_path
   when 'home'
     visit root_path
   when 'evaluations'
     visit evaluations_path
+  when 'send'
+    step 'I am an administrator'
+    visit to_send_templates_path
   else
     raise "Unknown page: #{page_name}"
   end
 end
 
-When(/^(?:I )?move to the(?: main)? (.*) page$/) do |page_name|
+When(/^(?:I )?move to the(?: main)? (\w+) page$/) do |page_name|
   step "I am on the #{page_name} page"
+end
+
+Then(/^(?:I )?should be on the (\w+) page$/) do |page_name|
+  case page_name.downcase
+  when 'management'
+    raise "Unhandled page (how do I know i'm here?): #{page_name}"
+  when 'home'
+    raise "Unhandled page (how do I know i'm here?): #{page_name}"
+  when 'evaluations'
+    raise "Unhandled page (how do I know i'm here?): #{page_name}"
+  when 'send'
+    expect(page).to have_css('#template_id')
+  else
+    raise "Unknown 'should be' for page: #{page_name}"
+  end
 end
