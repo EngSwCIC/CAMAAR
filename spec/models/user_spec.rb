155835e472606112cfcdb50d4bec95d21e8eda44
Mateus Santos <santos.mateus@protonmail.com>
2024-06-21 13:05:40 -0300

    expect find pending forms to exist

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a find_pending_forms function" do
+    @user = FactoryBot.create(:user)
+    expect(@user).to respond_to(:find_pending_forms)
+  end
 end
