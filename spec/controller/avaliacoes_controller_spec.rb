6bbdde8f7aa967532f0dc69f1543ae908220c96a
Mateus Santos <santos.mateus@protonmail.com>
2024-06-21 11:35:56 -0300

    created tests for avaliacoes controller

---
+++ spec/controller/avaliacoes_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe AvaliacoesController, type: :controller do
+  describe "Correct avaliacoes view" do
+    it "returns http success"
+    describe "gets all correct forms" do
+      it "gets forms"
+      it "gets forms from student"
+      it "gets pending forms"
+    end
+  end
+
+end
