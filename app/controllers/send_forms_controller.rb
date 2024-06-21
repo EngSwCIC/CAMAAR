87268319af3b4030d9edd19aa6ea48e8b5840019
Mateus Santos <santos.mateus@protonmail.com>
2024-06-21 20:58:52 -0300

    completed send form view

--- app/controllers/send_forms_controller.rb
+++ app/controllers/send_forms_controller.rb
@@ -1,5 +1,7 @@
 class SendFormsController < ApplicationController
     def index
+        @templates = Template.all
+        @study_classes = StudyClass.all
         render layout: "home"
     end
 end
