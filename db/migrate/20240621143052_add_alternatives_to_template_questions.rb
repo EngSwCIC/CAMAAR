8c8d0db04cfa585f6f38121df231ff7f23348b42
luizeduardoreis <luizedureis1504@gmail.com>
2024-06-21 18:24:21 -0300

    Fixed migrations

--- db/migrate/20240621143052_add_alternatives_to_template_questions.rb
+++ db/migrate/20240621143052_add_alternatives_to_template_questions.rb
@@ -1,5 +1,5 @@
 class AddAlternativesToTemplateQuestions < ActiveRecord::Migration[7.1]
   def change
-    remove_column :template_questions, :alternatives, :text
+    add_column :template_questions, :alternatives, :text
   end
 end
