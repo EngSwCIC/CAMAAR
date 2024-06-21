ffc67a471bf7bf967affdb61b9094c930656d261
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-21 14:52:54 -0300

    refactor: Generalize visit page step

--- features/step_definitions/page_steps.rb
+++ features/step_definitions/page_steps.rb
@@ -1,3 +1,12 @@
-Given(/^I am on the(?: main)? Management page$/) do
-  visit manager_path
+Given(/^I am on the(?: main)? (.*) page$/) do |page_name|
+  case page_name.downcase
+  when 'management'
+    visit manager_path
+  when 'home'
+    visit root_path
+  when 'evaluations'
+    visit evaluations_path
+  else
+    raise "Unknown page: #{page_name}"
+  end
 end
