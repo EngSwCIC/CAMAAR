74abb2ee08d31f864e82e50b3f97bf9b7e92f633
Dacam33c <62443759+Dacam33c@users.noreply.github.com>
2024-06-21 01:57:25 -0300

    testes adicionados

--- CAMAARAPP/features/login_steps.rb
+++ CAMAARAPP/features/login_steps.rb
@@ -7,3 +7,8 @@ When("I enter my correct email and password") do
     fill_in "email", with: "teste1@gmail.com"
     fill_in "senha", with: "teste1"
 end
+
+When("I click the "Login" button") do
+    driver.findelement (by.class.name("button")).click();
+    expect(driver.getCurrentUrl()).to eq('http://localhost:3000/user_home')
+end
