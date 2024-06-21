f2a712d320f8e8b87c2ba6cb209e5cbde82267c1
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-21 15:45:51 -0300

    test: Add declarative given step for importing

--- features/importing.feature
+++ features/importing.feature
@@ -1,7 +1,7 @@
 Feature: Importing data from SIGAA
 
-  Scenario: Importing for the first time
+  Scenario: Importing from SIGAA successfully
     Given I am logged in as an administrator
     And I am on the Management page
-    When I import data from SIGAA
+    When I click the "import" button
     Then the importing should be successful
--- features/step_definitions/importing_steps.rb
+++ features/step_definitions/importing_steps.rb
@@ -1,5 +1,11 @@
-When('I import data from SIGAA') do
+When('I click the "import" button') do
   click_button 'flow-first-import-button'
+end
+
+Given(/^(?:I|an administrator) imported data from SIGAA$/) do
+  step 'I am logged in as an administrator'
+  step 'I am on the Management page'
+  step 'I click the "import" button'
 end
 
 Then(/^the (?:importing|importation|import operation) should be successful$/) do
