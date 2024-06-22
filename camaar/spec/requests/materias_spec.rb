29547926639bfda1eb19e26566c6318c4bfa7491
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-06-22 00:05:35 +0000

    materias controller e alguns testes

--- camaar/spec/requests/materias_spec.rb
+++ camaar/spec/requests/materias_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Materias", type: :request do
   let!(:materia) { create(:materia) }
+  let(:materia_params) { { nome: "Materia de Teste", codigo: "TES0000" } }
 
   describe "obter Materias - GET #index" do
     it "foi bem sucedida" do
@@ -32,6 +33,26 @@ RSpec.describe "Materias", type: :request do
       expect(json["id"]).to eq(materia.id)
       expect(json["nome"]).to eq(materia.nome)
       expect(json["codigo"]).to eq(materia.codigo)
+    end
+  end
+  describe "GET #new" do
+    it "foi bem sucedida" do
+      get new_materia_path
+      expect(response).to be_successful
+    end
+  end
+  describe "POST #create" do
+    context "Parametros válidos" do
+      it "foi bem sucedida" do
+        post materias_path, params: { materia: materia_params }
+        expect(response).to redirect_to(materias_path)
+      end
+    end
+    context "Parametros inválidos" do
+      it "foi mal sucedida" do
+        post materias_path, params: {"oi": "tudo bom?"}
+        expect(response).to have_http_status(400)
+      end
     end
   end
 end
