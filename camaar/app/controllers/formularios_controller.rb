53f6073a82d66158c86635c810cba9b61cd3a6c4
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2024-06-21 22:33:56 -0300

    Hotifx para arrumar testes falhando (não deu muito certo)

--- camaar/app/controllers/formularios_controller.rb
+++ camaar/app/controllers/formularios_controller.rb
@@ -10,7 +10,7 @@ class FormulariosController < ApplicationController
         @formulario = Formulario.find(params[:id])
          # render json: formulario, status: :ok
     rescue StandardError => e
-        # render json: e, status: :not_found
+        render json: e, status: :not_found
     end
 
     def create
@@ -25,7 +25,7 @@ class FormulariosController < ApplicationController
         @formulario = Formulario.find(params[:id])
         if formulario.update!(formulario_params)
             redirect_to formularios_path, notice: "#{formulario.nome} updated."
-        else 
+        else
             flash.now[:alert] = "#{formulario.nome} could not be updated: " + formulario.errors.full_messages.join(", ")
             render 'edit', status: :unprocessable_entity
             # render json: formulario, status: :ok
@@ -37,16 +37,16 @@ class FormulariosController < ApplicationController
         formulario.destroy!
         # render json: { message: "Formulario deleted." }, status: :ok
     rescue StandardError => e
-        # render json: e, status: :not_found
+        render json: e, status: :not_found
     end
 
-    def new 
+    def new
         @formularios = Formulario.new
     end
 
     def export_csv
         @formularios = Formulario.all
-    
+
         respond_to do |format|
           format.csv { send_data generate_csv(@formularios), filename: "formularios-#{Date.today}.csv" }
         end
@@ -61,7 +61,7 @@ class FormulariosController < ApplicationController
     def generate_csv(formularios)
         CSV.generate(headers: true) do |csv|
           csv << ['ID', 'Nome', 'Descrição', 'Criado em', 'Atualizado em']
-    
+
           formularios.each do |formulario|
             csv << [formulario.id, formulario.nome, formulario.descricao, formulario.created_at, formulario.updated_at]
           end
