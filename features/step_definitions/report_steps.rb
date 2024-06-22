b3010ec5f18e97785160a922971804f130520b61
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-22 10:20:30 -0300

    test: Use generic  "am on page" step
    Replace hardcoded "visit" step with our more generic implementation
    
    Use expect(page).to have_text instead because .should syntax is
    deprecated.

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -1,42 +1,35 @@
-Given('I am in the evaluations screen') do
-    visit evaluations_path
-end
-
-Given('I am in the management screen') do
-    visit manager_path
-end
-
 Given('I have a form for discipline "Estrutura de Dados", with professor "DANIEL DE PAULA PORTO" and semester "2020.1"') do
-    Discipline.destroy_all
-    Form.destroy_all
+  Discipline.destroy_all
+  Form.destroy_all
 
-    discipline = Discipline.create(name: "Estrutura de Dados", code: "CIC0090", professor_registration: "000", semester_id: 1)
-    form = discipline.forms.create
-    form.questions.create(type: "MultipleChoiceQuestion", 
-                          label: "Como você avalia a clareza das explicações do professor durante as aulas?",
-                          format: "Muito clara|Clara|Nem clara, nem confusa|Pouco clara|Nada clara")
+  discipline = Discipline.create(name: 'Estrutura de Dados', code: 'CIC0090', professor_registration: '000',
+                                 semester_id: 1)
+  form = discipline.forms.create
+  form.questions.create(type: 'MultipleChoiceQuestion',
+                        label: 'Como você avalia a clareza das explicações do professor durante as aulas?',
+                        format: 'Muito clara|Clara|Nem clara, nem confusa|Pouco clara|Nada clara')
 
-    discipline.save
+  discipline.save
 end
 
 Given('I have no semesters registered') do
-    Semester.destroy_all
+  Semester.destroy_all
 end
 
-When("I click Baixar Resultados") do
-    visit "evaluations/1.csv"
+When('I click Baixar Resultados') do
+  visit 'evaluations/1.csv'
 end
 
-When("I click Gerar CSV de Resultados deste Semestre") do
-    visit "semesters/current.csv"
+When('I click Gerar CSV de Resultados deste Semestre') do
+  visit 'semesters/current.csv'
 end
 
-Then("I should download a CSV file for that discipline") do
+Then('I should download a CSV file for that discipline') do
 end
 
-Then("I should download a CSV file for the disciplines of that semester") do
+Then('I should download a CSV file for the disciplines of that semester') do
 end
 
 Then('I should see {string}') do |string|
-    page.should have_content(string)
+  expect(page).to have_text(string)
 end
